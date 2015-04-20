e854cb6b015563a69a59d793b1ef780c1532c5a2
Roc Yu <rociiu.yu@gmail.com>
2015-04-20 23:25:48 +0800

    add http auth for qa/staging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  if Rails.env.qa? || Rails.env.staging?
+    http_basic_authenticate_with name: "greatminds ", password: "intridea4gm"
+  end
 end
