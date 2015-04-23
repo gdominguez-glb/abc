7a7dc442f83dfcca07887424377035273c8dd6a5
Roc Yu <rociiu.yu@gmail.com>
2015-04-23 15:24:13 +0800

    GM-45 implement profile edit

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -9,4 +9,18 @@ class AccountController < ApplicationController
 
   def profile
   end
+
+  def save_profile
+    if spree_current_user.update(user_params)
+      redirect_to '/account/settings', notice: "Saved profile successfully"
+    else
+      render :edit_profile
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :address, :interested_subject, :interested_grade_level, :school_name, :heard_from)
+  end
 end
