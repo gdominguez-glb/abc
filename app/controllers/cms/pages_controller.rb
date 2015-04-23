3726cfe43edd19403270abb3ec878746eff11cd6
Andy Wang <wangyaodi@gmail.com>
2015-04-23 20:17:26 +0800

    allow to crud pages in cms admin

--- app/controllers/cms/pages_controller.rb
+++ app/controllers/cms/pages_controller.rb
@@ -28,7 +28,7 @@ class Cms::PagesController < Cms::BaseController
 
     respond_to do |format|
       if @page.save
-        format.html { redirect_to @page, notice: 'Page was successfully created.' }
+        format.html { redirect_to [:cms, @page], notice: 'Page was successfully created.' }
         format.json { render :show, status: :created, location: @page }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class Cms::PagesController < Cms::BaseController
   def update
     respond_to do |format|
       if @page.update(page_params)
-        format.html { redirect_to @page, notice: 'Page was successfully updated.' }
+        format.html { redirect_to [:cms, @page], notice: 'Page was successfully updated.' }
         format.json { render :show, status: :ok, location: @page }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class Cms::PagesController < Cms::BaseController
   def destroy
     @page.destroy
     respond_to do |format|
-      format.html { redirect_to pages_url, notice: 'Page was successfully destroyed.' }
+      format.html { redirect_to cms_pages_url, notice: 'Page was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,8 @@
 class Page < ActiveRecord::Base
+  validates :title, presence: true
+  validates :slug, presence: true, uniqueness: true
+  validates :group_name, presence: true
+
+
+
 end
