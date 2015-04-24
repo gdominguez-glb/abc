641b5e5ae5b5f50d96f664f243f5f0c63e89936a
Andy Wang <wangyaodi@gmail.com>
2015-04-24 16:43:36 +0800

    refine condition

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   def show
     @page = Page.find_by(slug: params[:slug]) || Page.find_by(slug: 'not-found')
     @sub_nav_items = Page.show_in_sub_navigation(@page.group_name)
-    if @page.try(:layout) && !@page.layout.blank?
+    if @page.layout.present?
       render layout: @page.layout
     end
   end
