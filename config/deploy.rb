e6602aeb5853b9a2a8ade45e713f46ac964479a1
Roc Yu <rociiu.yu@gmail.com>
2015-04-24 21:53:04 +0800

    add task to cap for run rake task directly with cap command in server

--- config/deploy.rb
+++ config/deploy.rb
@@ -60,4 +60,12 @@ namespace :deploy do
 
 end
 
+namespace :rake do
+  desc "Invoke rake task"
+  task :invoke do
+    run "cd #{deploy_to}/current"
+    run "bundle exec rake #{ENV['task']} RAILS_ENV=#{rails_env}"
+  end
+end
+
 after "deploy:finished", 'deploy:unicorn_restart'
