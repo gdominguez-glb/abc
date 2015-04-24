dada1b49c83c703461e6da1cf5be9d2242973efa
Roc Yu <rociiu.yu@gmail.com>
2015-04-24 22:18:25 +0800

    fix deploy:invoke task, add example command

--- config/deploy.rb
+++ config/deploy.rb
@@ -58,13 +58,16 @@ namespace :deploy do
   #   end
   # end
 
-end
-
-namespace :rake do
+  # example: cap qa deploy:invoke task=db:migrate
   desc "Invoke rake task"
   task :invoke do
-    run "cd #{deploy_to}/current"
-    run "bundle exec rake #{ENV['task']} RAILS_ENV=#{rails_env}"
+    on roles(:web) do
+      within "#{deploy_to}/current" do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, ENV['task']
+        end
+      end
+    end
   end
 end
 
