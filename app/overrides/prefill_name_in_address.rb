a80e7382e86549ab7bf5160de766d5ed338589b0
Eric Himmelreich <eric@intridea.com>
2015-04-28 16:04:05 -0400

    Prefill name in address

---
+++ app/overrides/prefill_name_in_address.rb
@@ -0,0 +1,20 @@
+Deface::Override.new(
+  virtual_path: 'spree/address/_form',
+  name: 'prefill_first_name_in_address',
+  replace_contents: "p.form-group:first",
+  text: <<-HTML
+          <%= form.label :firstname, Spree.t(:first_name) %><span class="required">*</span><br>
+          <%= form.text_field :firstname, :class => 'form-control required' , :value => current_spree_user.name.split(" ").first %>
+  HTML
+)
+
+
+Deface::Override.new(
+  virtual_path: 'spree/address/_form',
+  name: 'prefill_last_name_in_address',
+  replace_contents: "p.form-group:nth-child(2)",
+  text: <<-HTML
+          <%= form.label :lastname, Spree.t(:last_name) %><span class="required">*</span><br>
+          <%= form.text_field :lastname, :class => 'form-control required' , :value => current_spree_user.name.split(" ").last %>
+  HTML
+)
