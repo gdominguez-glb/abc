fb51fa4f3adeb022a07417110ebb41efde9660de
Roc Yu <rociiu.yu@gmail.com>
2015-04-28 18:29:07 +0800

    add slug to curriculum, add task to seed default curriculum data

--- db/migrate/20150428070955_create_spree_curriculums.rb
+++ db/migrate/20150428070955_create_spree_curriculums.rb
@@ -2,6 +2,7 @@ class CreateSpreeCurriculums < ActiveRecord::Migration
   def change
     create_table :spree_curriculums do |t|
       t.string :name
+      t.string :slug
       t.text :description
       t.integer :position
       t.datetime :deleted_at
