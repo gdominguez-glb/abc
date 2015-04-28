aeaf1097ad011fa2941554fcecd118cb4de7c0a7
Eric Himmelreich <eric@intridea.com>
2015-04-28 10:27:36 -0400

    Add license to new product

--- app/overrides/add_license_fields_to_product_admin.rb
+++ app/overrides/add_license_fields_to_product_admin.rb
@@ -17,3 +17,23 @@ Deface::Override.new(
           </div>
 "
 )
+
+Deface::Override.new(
+    virtual_path: "spree/admin/products/new",
+    name: "add_license_fields_to_product_new",
+    insert_bottom: "[data-hook='new_product_attrs']",
+    text: "
+          <div data-hook='new_product_license' class='col-md-4'>
+            <div class='form-group field' id='product_license_text_field'>
+              <%= f.label :license_text %>
+              <%= f.text_area :license_text, class: 'form-control' %>
+            </div>
+          </div>
+          <div data-hook='admin_product_form_license_length' class='col-md-4'>
+            <div class='form-group field' id='product_license_length_field'>
+              <%= f.label :license_length_in_days %>
+              <%= f.number_field :license_length, class: 'form-control' %>
+            </div>
+          </div>
+"
+)
