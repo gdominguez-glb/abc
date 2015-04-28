7aa1f47bcf823ba017bf0c2bdeb0b713e892d9e7
Roc Yu <rociiu.yu@gmail.com>
2015-04-28 09:30:40 +0800

    use allow_blank instead of if proc in validation, cleanup old hash syntax

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,5 +1,4 @@
 Spree::Product.class_eval do
   validates :license_length, numericality: { only_integer: true }
-  validates :redirect_url, format: { with: URI.regexp },
-    if: Proc.new { |a| a.redirect_url.present? }
+  validates :redirect_url, format: { with: URI.regexp }, allow_blank: true
 end
--- app/overrides/add_name_to_user_registration.rb
+++ app/overrides/add_name_to_user_registration.rb
@@ -1,8 +1,10 @@
-Deface::Override.new(:virtual_path => 'spree/shared/_user_form',
-  :name => 'add_name_to_user_registration',
-  :insert_top => "#password-credentials",
-  :text => "
+Deface::Override.new(
+  virtual_path: 'spree/shared/_user_form',
+  name: 'add_name_to_user_registration',
+  insert_top: "#password-credentials",
+  text: "
       <div class='form-group'>
         <%= f.text_field :name, class: 'form-control', placeholder: 'Name' %>
       </div>
-  ")
+  "
+)
