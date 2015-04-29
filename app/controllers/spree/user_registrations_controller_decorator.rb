208341c24e16f38db97d3b733c28fc033128b9e6
Eric Himmelreich <eric@intridea.com>
2015-04-29 13:39:23 -0400

    Redirect user after sign in / up

--- app/controllers/spree/user_registrations_controller_decorator.rb
+++ app/controllers/spree/user_registrations_controller_decorator.rb
@@ -1,4 +1,17 @@
 Spree::UserRegistrationsController.class_eval do
+  def after_sign_in_path_for(resource)
+    if resource
+      if !resource.admin?
+        return_to = session["spree_user_return_to"]
+        session["spree_user_return_to"] = nil
+
+        return_to || '/store'
+      else
+        '/store/admin'
+      end
+    end
+  end
+
   private
     def spree_user_params
       _params = params.require(:spree_user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :school_district_id, school_district_attributes: [:name, :state_id, :place_type])
