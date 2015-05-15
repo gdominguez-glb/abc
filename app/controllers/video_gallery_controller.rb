63473ce953c66058c3e0a2637e813c1f867e8833
Roc Yu <rociiu.yu@gmail.com>
2015-05-15 09:17:50 +0800

    GM-116 update video gallery filters to use taxonomy/taxons for categories/tags filter

--- app/controllers/video_gallery_controller.rb
+++ app/controllers/video_gallery_controller.rb
@@ -4,6 +4,9 @@ class VideoGalleryController < ApplicationController
   helper_method :bought_product?, :preference_video_player, :can_play_video?, :favorited_product?, :can_favorite_product?
 
   def index
+    params[:taxon_ids] ||= []
+    @taxonomies = Spree::Taxonomy.includes(root: :children)
+
     @video_products = Spree::Product.videos.page(params[:page]).per(params[:per_page])
     @video_products = filter_video_products(@video_products)
 
@@ -47,20 +50,15 @@ class VideoGalleryController < ApplicationController
   def load_filter_data
     @grades = Spree::Grade.order('position asc')
     @selected_grade = Spree::Grade.find_by(id: params[:grade_id])
-
-    @curriculums = Spree::Curriculum.order('position asc')
-    @selected_curriculum = Spree::Curriculum.find_by(id: params[:curriculum_id])
   end
 
   def filter_video_products(video_products)
     if params[:query].present?
       video_products = video_products.where("name like ?", "%#{params[:query]}%")
     end
-    if params[:grade_id].present?
-      video_products = video_products.where(grade_id: params[:grade_id])
-    end
-    if params[:curriculum_id].present?
-      video_products = video_products.where(curriculum_id: params[:curriculum_id])
+    if params[:taxon_id].present?
+      @taxon = Spree::Taxon.friendly.find(params[:taxon_id])
+      video_products = video_products.in_taxon(@taxon) if @taxon
     end
     video_products
   end
