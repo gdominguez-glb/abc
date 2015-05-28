b953667186d9697982de0ffff6f31fbc2c5da48b
Roc Yu <rociiu.yu@gmail.com>
2015-05-28 18:39:49 +0800

    GM-28 implement search box, search against product and pages

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,5 @@
 class SearchController < ApplicationController
   def index
+    @result = Page.search(params[:query], index_name: [Spree::Product.searchkick_index.name, Page.searchkick_index.name])
   end
 end
