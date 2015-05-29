8c574830406e6b05b8d5ae8de6f1220eac1c1abd
Roc Yu <rociiu.yu@gmail.com>
2015-05-29 17:30:35 +0800

    GM-141 exclude other taxons if taxonomy not allow multiple taxons selected in video gallery filters

--- app/helpers/video_gallery_helper.rb
+++ app/helpers/video_gallery_helper.rb
@@ -1,13 +1,19 @@
 module VideoGalleryHelper
   include Spree::BaseHelper
 
-  def video_gallery_taxons_tree(root_taxon, current_taxon, max_level = 1)
-    return '' if max_level < 1 || root_taxon.leaf?
+  def video_gallery_taxons_tree(root_taxon, current_taxon, allow_multiple_taxons_selected)
     content_tag :div, class: 'list-group', style: 'display: none;' do
+      sibling_ids = root_taxon.children.map(&:id)
       root_taxon.children.map do |taxon|
         css_class = (params[:taxon_ids].include?(taxon.id.to_s)) ? 'list-group-item active' : 'list-group-item'
-        link_to(taxon.name, params.merge(taxon_ids: params[:taxon_ids] + [taxon.id]), class: css_class) + video_gallery_taxons_tree(taxon, current_taxon, max_level - 1)
+        taxon_ids = generate_taxon_ids_param(taxon, params[:taxon_ids], sibling_ids, allow_multiple_taxons_selected)
+        link_to(taxon.name, params.merge(taxon_ids: taxon_ids), class: css_class)
       end.join("\n").html_safe
     end
+  end
+
+  def generate_taxon_ids_param(taxon, taxon_ids, sibling_ids, allow_multiple_taxons_selected)
+    return taxon_ids + [taxon.id] if allow_multiple_taxons_selected
+    taxon_ids.reject{|_id| sibling_ids.include?(_id.to_i)} + [taxon.id]
   end
 end
