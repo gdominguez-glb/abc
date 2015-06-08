60b803b23d37df7eda071c21a0457dcfdcb67330
Roc Yu <rociiu.yu@gmail.com>
2015-06-08 09:48:04 +0800

    refactor video gallery helper to reduce complexity

--- app/helpers/video_gallery_helper.rb
+++ app/helpers/video_gallery_helper.rb
@@ -5,11 +5,15 @@ module VideoGalleryHelper
     content_tag :div, class: 'list-group', style: 'display: none;' do
       sibling_ids = root_taxon.children.map(&:id)
       root_taxon.children.map do |taxon|
-        css_class = (params[:taxon_ids].include?(taxon.id.to_s)) ? 'list-group-item active' : 'list-group-item'
-        taxon_ids = generate_taxon_ids_param(taxon, params[:taxon_ids], sibling_ids, allow_multiple_taxons_selected)
-        link_to(taxon.name, params.merge(taxon_ids: taxon_ids), class: css_class)
+        video_gallery_taxon_item(taxon, sibling_ids, allow_multiple_taxons_selected)
       end.join("\n").html_safe
     end
+  end
+
+  def video_gallery_taxon_item(taxon, sibling_ids, allow_multiple_taxons_selected)
+    css_class = (params[:taxon_ids].include?(taxon.id.to_s)) ? 'list-group-item active' : 'list-group-item'
+    taxon_ids = generate_taxon_ids_param(taxon, params[:taxon_ids], sibling_ids, allow_multiple_taxons_selected)
+    link_to(taxon.name, params.merge(taxon_ids: taxon_ids), class: css_class)
   end
 
   def generate_taxon_ids_param(taxon, taxon_ids, sibling_ids, allow_multiple_taxons_selected)
