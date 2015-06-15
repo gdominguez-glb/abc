9760bd2d8f626f7fbedd6abc871ddd3fdc7e951d
Roc Yu <rociiu.yu@gmail.com>
2015-06-15 20:14:36 +0800

    GM-67 add scraper to fetch publications posts from medium

---
+++ lib/medium_scraper.rb
@@ -0,0 +1,35 @@
+require 'httparty'
+
+class MediumScraper
+  PUBLICATIONS = [
+    'https://medium.com/eureka-math',
+    'https://medium.com/eureka-stories',
+    'https://medium.com/wheatley-blog',
+    'https://medium.com/great-minds-press',
+    'https://medium.com/great-minds-reports'
+  ]
+
+  def scape_publications
+    PUBLICATIONS.each do |publication_url|
+      begin
+        response = HTTParty.get(publication_url + '/latest', query: { format: 'json' }, parser: nil)
+        publication_data = convert_to_json(response.body)
+        publication_data['payload']['posts'].each do |post|
+          slug            = post['slug']
+          post_url        = publication_url + '/' + slug + '-' + post['id']
+          post_response   = HTTParty.get(post_url, query: { format: 'json' }, parser: nil)
+          post_data       = convert_to_json(post_response.body)
+          post_paragraphs = post_data['payload']['value']['content']['bodyModel']['paragraphs']
+          puts post_paragraphs.count
+        end
+      rescue
+      end
+    end
+  end
+
+  def convert_to_json(response_body)
+    JSON.parse(response_body[16..-1])
+  rescue
+    {}
+  end
+end
