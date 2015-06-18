b25341495d4bbc817a015116446d9d1b0aa0774f
Roc Yu <rociiu.yu@gmail.com>
2015-06-18 19:51:39 +0800

    use http on s3 protocol to fix 403 warning on access s3 file, remove unneeded spree images

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,6 +2,7 @@ class Document < ActiveRecord::Base
   has_attached_file :attachment, {
     path:           "/:class/:attachment/:id_partition/:style/:filename",
     url:            ":s3_alias_url",
+    s3_protocol:    "http",
     s3_host_alias:  ENV['s3_bucket_name']
   }
 
--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -38,7 +38,7 @@ attachment_config = {
 
   storage:        :s3,
   s3_headers:     { "Cache-Control" => "max-age=31557600" },
-  s3_protocol:    "https",
+  s3_protocol:    "http",
   bucket:         ENV['s3_bucket_name'],
   url:            ":s3_domain_url",
 
