ede81f7fa4df6417c0dfcf9426d8536b13055add
Roc Yu <rociiu.yu@gmail.com>
2015-06-18 15:11:52 +0800

    GM-67 fix codeclimate issue on account.js

--- app/assets/javascripts/account.js
+++ app/assets/javascripts/account.js
@@ -6,14 +6,18 @@ $(function(){
       var context = this, args = arguments;
       var later = function() {
         timeout = null;
-        if (!immediate) func.apply(context, args);
+        if (!immediate) {
+          func.apply(context, args);
+        }
       };
       var callNow = immediate && !timeout;
       clearTimeout(timeout);
       timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
+      if (callNow) {
+        func.apply(context, args);
+      }
     };
-  };
+  }
 
   var updateAccountBackground = debounce(function() {
     if ($(".recent-history").outerHeight() > $(".account .content").outerHeight()) {
