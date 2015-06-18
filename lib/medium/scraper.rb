bfe94ac536df5a106e24d8bfdc80ff7a27b5dd91
Roc Yu <rociiu.yu@gmail.com>
2015-06-18 15:24:04 +0800

    GM-67 refactor Medium::Scrapper to cleanup complexity to fix codeclimate issue

--- lib/medium/scraper.rb
+++ lib/medium/scraper.rb
@@ -1,22 +1,32 @@
 module Medium
   class Scraper
     def scrape_publications
-      MediumPublication.all.each do |publication|
+      MediumPublication.find_each do |publication|
         begin
-          response = HTTParty.get(publication.url + '/latest', query: { format: 'json' }, parser: nil)
-          publication_data = convert_to_json(response.body)
+          publication_data = request_data_from_medium(publication.url + '/latest')
           publication_data['payload']['posts'].each do |post|
-            slug          = post['slug']
-            post_url      = publication.url + '/' + slug + '-' + post['id']
-            post_response = HTTParty.get(post_url, query: { format: 'json' }, parser: nil)
-            post_data     = convert_to_json(post_response.body)
-            post_hash     = PostProcessor.new(data: post_data).process
-            post          = Post.find_or_initialize_by(medium_id: post_hash[:medium_id])
-            post.update(post_hash.merge(medium_publication_id: publication.id))
+            import_post_from_medium(publication, post)
           end
         rescue
         end
       end
+    end
+
+    def import_post_from_medium(publication, post_json)
+      post_url  = construct_post_url(publication, post_json)
+      post_data = request_data_from_medium(post_url)
+      post_hash = PostProcessor.new(data: post_data).process
+      post      = Post.find_or_initialize_by(medium_id: post_hash[:medium_id])
+      post.update(post_hash.merge(medium_publication_id: publication.id))
+    end
+
+    def request_data_from_medium(url)
+      response = HTTParty.get(url, query: { format: 'json' }, parser: nil)
+      convert_to_json(response.body)
+    end
+
+    def construct_post_url(publication, post)
+      publication.url + '/' + post['slug'] + '-' + post['id']
     end
 
     def convert_to_json(response_body)
