465e8c17764ee960d26176170001485e090632e6
Roc Yu <rociiu.yu@gmail.com>
2015-06-20 07:55:20 +0800

    fix exception when medium publication slug is nil

--- app/models/medium_publication.rb
+++ app/models/medium_publication.rb
@@ -1,5 +1,5 @@
 class MediumPublication < ActiveRecord::Base
-  validates_presence_of :title, :url
+  validates_presence_of :title, :url, :slug
 
   BLOG_TYPES = ['global', 'curriculum']
 
@@ -7,6 +7,6 @@ class MediumPublication < ActiveRecord::Base
   belongs_to :page
 
   BLOG_TYPES.each do |blog_type|
-    scope blog_type, -> { where(display: true).where(blog_type: blog_type).order('position asc') }
+    scope blog_type, -> { where(display: true).where(blog_type: blog_type).where("slug is not null").order('position asc') }
   end
 end
