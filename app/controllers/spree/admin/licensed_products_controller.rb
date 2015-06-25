dd54ffe5e5985f16821f207b803adcacffd05a50
Roc Yu <rociiu.yu@gmail.com>
2015-06-25 17:00:35 +0800

    GM-181 implement form to create license in spree admin

--- app/controllers/spree/admin/licensed_products_controller.rb
+++ app/controllers/spree/admin/licensed_products_controller.rb
@@ -4,6 +4,10 @@ module Spree
       def index
         @licensed_products = Spree::LicensedProduct.includes([:user, :product]).page(params[:page])
       end
+
+      def new
+        @licensed_product = Spree::LicensedProduct.new
+      end
     end
   end
 end
--- app/models/spree/licensed_product.rb
+++ app/models/spree/licensed_product.rb
@@ -5,6 +5,11 @@ class Spree::LicensedProduct < ActiveRecord::Base
 
   scope :available, -> { where("expire_at > ?", Time.now) }
 
+  validates_presence_of :product, :user
+  validates_numericality_of :quantity, :greater_than => 0
+
+  before_create :set_expire_at
+
   def distribute_license(user_or_email, quantity=1)
     distribution_attrs = {
       licensed_product: self,
@@ -18,5 +23,13 @@ class Spree::LicensedProduct < ActiveRecord::Base
       distribution_attrs[:email] = user_or_email
     end
     Spree::ProductDistribution.create(distribution_attrs)
+  end
+
+  private
+
+  def set_expire_at
+    if self.product.license_length.present? && self.expire_at.blank?
+      self.expire_at = product.license_length.days.since(Time.now)
+    end
   end
 end
