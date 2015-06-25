f6e6b31ba229d45c56a3345e3c151eac77cc4e65
Roc Yu <rociiu.yu@gmail.com>
2015-06-25 17:59:19 +0800

    Gm-181 implement multiple products select in create new license

--- app/controllers/spree/admin/licensed_products_controller.rb
+++ app/controllers/spree/admin/licensed_products_controller.rb
@@ -8,6 +8,24 @@ module Spree
       def new
         @licensed_product = Spree::LicensedProduct.new
       end
+
+      def create
+        product_ids = params[:licensed_product_product_ids].split(',')
+        licensed_products = product_ids.map do |product_id|
+          Spree::LicensedProduct.new(permitted_resource_params.merge(product_id: product_id))
+        end
+        if licensed_products.blank?
+          flash[:error] = 'Must select products'
+          render :new and return
+        end
+        if licensed_products.all?{|lp| lp.valid? }
+          licensed_products.each{|lp| lp.save}
+          redirect_to spree.admin_licensed_products_path
+        else
+          @licensed_product = licensed_products.first
+          render :new
+        end
+      end
     end
   end
 end
