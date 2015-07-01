66a1a6c2d2da1504cc777c40f4ccf01d777f655b
Roc Yu <rociiu.yu@gmail.com>
2015-07-01 19:13:38 +0800

    add test to Cms::PagesController

--- spec/controllers/cms/pages_controller_spec.rb
+++ spec/controllers/cms/pages_controller_spec.rb
@@ -1,5 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe Cms::PagesController, type: :controller do
+  let(:page) { create(:page) }
 
+  login_admin
+
+  describe "GET 'index'" do
+    it "success" do
+      get :index
+      expect(response).to be_success
+      expect(assigns(:pages)).not_to be_nil
+    end
+  end
+
+  describe "GET 'show'" do
+
+    it "success" do
+      get :show, id: page.id
+      expect(response).to be_success
+      expect(assigns(:page)).not_to be_nil
+    end
+  end
+
+  describe "GET 'new'" do
+    it "success" do
+      get :new
+      expect(response).to be_success
+      expect(assigns(:page)).not_to be_nil
+    end
+  end
+
+  describe "GET 'edit'" do
+    it "success" do
+      get :edit, id: page.id
+      expect(response).to be_success
+      expect(assigns(:page)).not_to be_nil
+    end
+  end
+
+  describe "PUT 'update'" do
+    it "update successfully" do
+      put :update, id: page.id, page: { title: 'new title' }
+      expect(response).to be_redirect
+    end
+
+    it "fail to update" do
+      put :update, id: page.id, page: { title: '' }
+      expect(response).to render_template(:edit)
+    end
+  end
+
+  describe "DELETE 'destroy'" do
+    it "destroy successfully" do
+      delete :destroy, id: page.id
+      expect(response).to be_redirect
+      expect(Page.find_by(id: page.id)).to be_nil
+    end
+  end
 end
