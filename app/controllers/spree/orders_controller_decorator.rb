6338939ff91b8400e79de7a2ad55709fe7c0c49a
Roc Yu <rociiu.yu@gmail.com>
2015-07-02 11:27:25 +0800

    fix failure in orders controller

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -1,6 +1,6 @@
 Spree::OrdersController.class_eval do
   def update_simple_cart
-    @order = current_order || Order.incomplete.find_or_initialize_by(guest_token: cookies.signed[:guest_token])
+    @order = current_order || Spree::Order.incomplete.find_or_initialize_by(guest_token: cookies.signed[:guest_token])
     @order.contents.update_cart(order_params)
   end
 end
