927004e953550ed5b02c5ccf12b475eb3c03879a
Roc Yu <rociiu.yu@gmail.com>
2015-07-03 09:22:15 +0800

    refactor: extract namespace routes into individual files to keep organized

---
+++ config/routes/cms.rb
@@ -0,0 +1,25 @@
+namespace :cms do
+  root 'dashboard#index'
+  get '/dashboard', to: 'dashboard#index', as: :dashboard
+
+  get '/user_profile_settings', to: 'user_profile_settings#index'
+  post '/user_profile_settings', to: 'user_profile_settings#save'
+
+  get 'products/search', to: 'products#search'
+
+  resources :pages
+  resources :documents
+  resources :questions
+  resources :contacts, only: [:index, :destroy, :edit, :update]
+  resources :recommendations
+  resources :medium_publications do
+    collection do
+      post :trigger_medium_importer
+    end
+  end
+  resources :contact_topics do
+    collection do
+      post :update_positions
+    end
+  end
+end
