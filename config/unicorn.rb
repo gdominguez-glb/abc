278a7afecccec84dcb3986e9517abc9614b3075c
Roc Yu <rociiu.yu@gmail.com>
2015-07-09 09:25:10 +0800

    update unicorn.rb to kill old unicorn pid after start new instance

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,6 +10,8 @@ working_directory "/var/www/greatminds/current"
 # pid "/path/to/pids/unicorn.pid"
 pid "/var/www/greatminds/current/tmp/pids/unicorn.pid"
 
+preload_app true
+
 # Path to logs
 # stderr_path "/path/to/log/unicorn.log"
 # stdout_path "/path/to/log/unicorn.log"
@@ -25,3 +27,29 @@ worker_processes 2
 
 # Time-out
 timeout 30
+
+before_fork do |server, worker|
+  # Disconnect since the database connection will not carry over
+  if defined? ActiveRecord::Base
+    ActiveRecord::Base.connection.disconnect!
+  end
+
+  # Quit the old unicorn process
+  old_pid = "/var/www/greatminds/current/tmp/pids/unicorn.pid.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+
+  sleep 1
+end
+
+after_fork do |server, worker|
+  # Start up the database connection again in the worker
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+end
