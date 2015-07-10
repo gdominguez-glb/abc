384aca30b1395b6ab30579217fa36db8f35e0533
Roc Yu <rociiu.yu@gmail.com>
2015-07-10 11:00:15 +0800

    GM-217 add history activity when user receive notification

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe Notification, type: :model do
   it { should belong_to(:notification_trigger) }
   it { should belong_to(:user).class_name('Spree::User') }
 
-  let(:notification) { create(:notification, read: false) }
+  let(:user) { create(:gm_user) }
+  let(:notification) { create(:notification, read: false, content: 'hello', user: user) }
 
   describe ".unread" do
     it "return unread notifications" do
@@ -17,6 +18,15 @@ RSpec.describe Notification, type: :model do
     it "mark notificaiton as read" do
       notification.mark_as_read!
       expect(notification.reload.read).to eq(true)
+    end
+  end
+
+  describe "log activity" do
+    it "create activity on notification user" do
+      notification
+      activity = user.activities.last
+      expect(activity.title).to eq(notification.content)
+      expect(activity.item).to eq(notification)
     end
   end
 end
