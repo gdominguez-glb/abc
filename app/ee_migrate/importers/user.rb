eebb5448b630da0e3d868f702c1a6b96c72708bf
Roc Yu <rociiu.yu@gmail.com>
2015-07-15 20:37:53 +0800

    add ee_id to ee users import to keep reference

--- app/ee_migrate/importers/user.rb
+++ app/ee_migrate/importers/user.rb
@@ -21,13 +21,15 @@ module Importers
         'exp_channel_data.field_id_158 as last_name'
       ]).joins("left join exp_member_data ON exp_members.member_id = exp_member_data.member_id")
         .joins("left join exp_channel_titles ON exp_channel_titles.author_id = exp_members.member_id")
-        .joins("left join exp_channel_data ON exp_channel_titles.entry_id = exp_channel_data.entry_id group by exp_members.member_id")
+        .joins("left join exp_channel_data ON exp_channel_titles.entry_id = exp_channel_data.entry_id")
         .group("exp_members.member_id").find_each do |member|
           user = Spree::User.find_or_initialize_by(email: member.email)
-          user.update(
+          user.assign_attributes(
+            ee_id: member.member_id,
             first_name: member.first_name,
             last_name: member.last_name
           )
+          user.save(validate: false)
         end
     end
   end
