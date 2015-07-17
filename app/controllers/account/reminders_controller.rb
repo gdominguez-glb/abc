8df18e1ef36f4c6fed0b6df0d2ef546e47159257
Roc Yu <rociiu.yu@gmail.com>
2015-07-17 20:02:45 +0800

    GM-12 implement validation on reminder form, integrate reminder deliver with modal

--- app/controllers/account/reminders_controller.rb
+++ app/controllers/account/reminders_controller.rb
@@ -1,8 +1,21 @@
 class Account::RemindersController < Account::BaseController
   def new
-    @reminder = ReminderForm.new
+    @reminder = ReminderForm.new(recipient: params[:email])
   end
 
   def create
+    @reminder = ReminderForm.new(reminder_params.merge(from_user: current_spree_user))
+    if @reminder.valid?
+      @reminder.perform
+      @success = true
+    else
+      @full_error_message = @reminder.errors.full_messages.join(" ")
+    end
+  end
+
+  private
+
+  def reminder_params
+    params.require(:reminder_form).permit(:recipient, :content)
   end
 end
--- app/models/reminder_form.rb
+++ app/models/reminder_form.rb
@@ -1,5 +1,12 @@
 class ReminderForm
   include ActiveModel::Model
 
-  attr_accessor :recipient, :content
+  attr_accessor :from_user, :recipient, :content
+
+  validates_presence_of :content
+  validates :recipient, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+
+  def perform
+    ReminderMailer.remind(@from_user, @recipient, @content).deliver
+  end
 end
