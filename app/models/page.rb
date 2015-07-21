f6f7084990489cc2ab459cbb37e7f813ad8daeb6
Roc Yu <rociiu.yu@gmail.com>
2015-07-21 16:38:50 +0800

    try to improve page score in codeclimate

--- app/models/page.rb
+++ app/models/page.rb
@@ -5,36 +5,18 @@ class Page < ActiveRecord::Base
   belongs_to :curriculum
 
   has_many :medium_publications
-
   has_one :event_page, ->{ where(display: true) }
 
-  validates :title, presence: true
   validates :slug, presence: true, uniqueness: true
-  validates :group_name, presence: true
+  validates_presence_of :title, :group_name
 
-  scope :visibles, -> {
-    where(visible: true).order('position ASC')
-  }
-  scope :curriculum_nav, -> {
-    show_in_top_navigation.joins(:curriculum).where({curriculums: { visible: true }})
-  }
-  scope :group_roots, -> {
-    where(group_root: true)
-  }
-  scope :not_group_roots, -> {
-    where(group_root: false)
-  }
-  scope :show_in_top_navigation, -> {
-    visibles.group_roots.where(show_in_nav: true)
-  }
-  scope :show_in_sub_navigation, -> (group_name) {
-    visibles.not_group_roots.where(group_name: group_name, show_in_nav: true)
-  }
-  scope :show_in_footer_as_top_links, -> {
-    visibles.group_roots.where(show_in_footer: true)
-  }
-  scope :show_in_footer_as_subgroup_links, -> (group_name) {
-    visibles.not_group_roots.where(show_in_footer: true, group_name: group_name)
-  }
+  scope :visibles, -> { where(visible: true).order('position ASC') }
+  scope :curriculum_nav, -> { show_in_top_navigation.joins(:curriculum).where({curriculums: { visible: true }}) }
+  scope :group_roots, -> { where(group_root: true) }
+  scope :not_group_roots, -> { where(group_root: false) }
+  scope :show_in_top_navigation, -> { visibles.group_roots.where(show_in_nav: true) }
+  scope :show_in_sub_navigation, -> (group_name) { visibles.not_group_roots.where(group_name: group_name, show_in_nav: true) }
+  scope :show_in_footer_as_top_links, -> { visibles.group_roots.where(show_in_footer: true) }
+  scope :show_in_footer_as_subgroup_links, -> (group_name) { visibles.not_group_roots.where(show_in_footer: true, group_name: group_name) }
 
 end
