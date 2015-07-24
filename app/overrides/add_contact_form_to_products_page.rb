e2a1d2003706276975d3dc3bbefa60818f758b29
Bryan Cunningham <bryan.cunningham@me.com>
2015-07-24 10:22:08 -0400

    Add callout banner to products page

---
+++ app/overrides/add_contact_form_to_products_page.rb
@@ -0,0 +1,19 @@
+Deface::Override.new(
+    virtual_path: "spree/shared/_products",
+    name: "add_contact_form_banner_to_products_page",
+    insert_before: "div#products",
+    text: '
+    <div class="call-banner green-bg">
+      <h4>Bulk Order? Mixed Order?</h4>
+      <h6>Our experienced representatives are standing by to help you with your special order.
+        <a href="" data-toggle="modal" data-target="#productContactForm">Contact us</a> today
+      <h6>
+    </div>'
+)
+
+Deface::Override.new(
+    virtual_path: "spree/shared/_products",
+    name: "add_contact_form_to_products_page",
+    insert_bottom: "div#products",
+    partial: 'spree/products/contact-modal.html'
+)
