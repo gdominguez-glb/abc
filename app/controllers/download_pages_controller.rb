53210da170472d5ba4d23d2f3f7963c0a4afccb7
Roc Yu <rociiu.yu@gmail.com>
2015-07-31 19:29:25 +0800

    GM-37 implement download button on sub material in product download page

--- app/controllers/download_pages_controller.rb
+++ app/controllers/download_pages_controller.rb
@@ -1,8 +1,20 @@
 class DownloadPagesController < ApplicationController
+  before_action :find_download_page
+
   def show
-    @download_page = DownloadPage.find_by(slug: params[:slug])
     redirect_to root_path if @download_page.blank?
 
     @products = @download_page.products
+  end
+
+  def download
+    material = Spree::Material.find(params[:material_id])
+    redirect_to material.material_files.first.file.url
+  end
+
+  private
+
+  def find_download_page
+    @download_page = DownloadPage.find_by(slug: params[:slug])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -58,7 +58,9 @@ Rails.application.routes.draw do
   end
 
   get '/:page_slug/events', to: 'events#page', as: :page_events
+
   get '/download_pages/:slug', to: 'download_pages#show', as: :download_page
+  get '/download_pages/:slug/download', to: 'download_pages#download', as: :download_material
 
   get '*slug', to: 'pages#show', constraints: lambda { |request| !(request.path =~ /\/(assets|store).*/) }
 end
