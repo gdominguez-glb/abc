cb4aa04f30a1fb560be967cb0422472cf1f7cbd7
Roc Yu <rociiu.yu@gmail.com>
2015-08-05 15:05:30 +0800

    GM-37 restrict s3 file on material file to private

--- app/models/spree/material_file.rb
+++ app/models/spree/material_file.rb
@@ -1,6 +1,6 @@
 class Spree::MaterialFile < ActiveRecord::Base
   belongs_to :material, class_name: 'Spree::Material'
 
-  has_attached_file :file
+  has_attached_file :file, s3_permissions: :private
   validates_attachment :file, presence: true, content_type: { content_type: /\A*\Z/ }
 end
