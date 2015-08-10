3090b0372f28c50fd81ad59c146f885ebb3d1544
Roc Yu <rociiu.yu@gmail.com>
2015-08-10 19:14:15 +0800

    GM-334 add material_files_count to materials to implement counter_cache

--- app/models/spree/material_file.rb
+++ app/models/spree/material_file.rb
@@ -1,5 +1,5 @@
 class Spree::MaterialFile < ActiveRecord::Base
-  belongs_to :material, class_name: 'Spree::Material'
+  belongs_to :material, class_name: 'Spree::Material', counter_cache: true
 
   has_attached_file :file, s3_permissions: :private
   validates_attachment :file, presence: true, content_type: { content_type: /\A*\Z/ }
