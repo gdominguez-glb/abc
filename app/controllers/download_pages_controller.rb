5b4d89ecc335baf6a29e2dcb74742e9519aeb209
Roc Yu <rociiu.yu@gmail.com>
2015-08-11 10:29:45 +0800

    GM-334 open product/materials based on track history

--- app/controllers/download_pages_controller.rb
+++ app/controllers/download_pages_controller.rb
@@ -6,8 +6,13 @@ class DownloadPagesController < ApplicationController
   def show
     redirect_to root_path if @download_page.blank?
 
-    @products = @download_page.products
-    @boughted_products = current_spree_user.products.where(id: @products.map(&:id))
+    @products            = @download_page.products
+    @boughted_products   = current_spree_user.products.where(id: @products.map(&:id))
+
+    product_tracks       = current_spree_user.product_tracks.where(product_id: @products.map(&:id))
+
+    @opened_product_ids  = product_tracks.map(&:product_id)
+    @opened_material_ids = product_tracks.map(&:material).compact.map(&:self_and_ancestors).flatten.map(&:id).uniq
   end
 
   private
