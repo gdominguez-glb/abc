bcb7d4532e1eead8a3f2171f266a670a590a6493
Adam Frazin <adam@mobomo.com>
2015-08-13 12:19:09 -0400

    Add comments to SalesforceReference

--- app/models/salesforce_reference.rb
+++ app/models/salesforce_reference.rb
@@ -4,11 +4,13 @@ class SalesforceReference < ActiveRecord::Base
 
   serialize :object_properties
 
+  # Gets a Salesforce object from specified (or cached) Salesforce properties
   def object_from_properties(properties = object_properties)
     return nil if properties.blank?
     Hashie::Mash.new(properties)
   end
 
+  # Updates the cached Salesforce properties from a given Salesforce object
   def update_properties_from_object(sfo)
     update(:object_properties, sfo.to_hash)
   end
