b0420be29957da57fd02a70675b596d33d6b4d78
Roc Yu <rociiu.yu@gmail.com>
2015-08-13 11:15:09 +0800

    GM-363 set header on material file to avoid browser open file instead of download

--- app/models/spree/material_file.rb
+++ app/models/spree/material_file.rb
@@ -1,6 +1,6 @@
 class Spree::MaterialFile < ActiveRecord::Base
   belongs_to :material, class_name: 'Spree::Material', counter_cache: true
 
-  has_attached_file :file, s3_permissions: :private
+  has_attached_file :file, s3_permissions: :private, s3_headers: { "Content-Disposition" => "attachment" }
   validates_attachment :file, presence: true, content_type: { content_type: /\A*\Z/ }
 end
