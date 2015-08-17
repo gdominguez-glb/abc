a6ba8a0dd729468500028094ba729ecb4fdd2a30
Roc Yu <rociiu.yu@gmail.com>
2015-08-17 18:01:31 +0800

    refactor download pages controller by extract method from show action (improve codeclimate score)

--- app/controllers/download_pages_controller.rb
+++ app/controllers/download_pages_controller.rb
@@ -9,13 +9,7 @@ class DownloadPagesController < ApplicationController
     @products            = @download_page.products
     @boughted_products   = current_spree_user.products.where(id: @products.map(&:id))
 
-    current_spree_user.product_tracks.find_or_create_by(product_id: params[:opened_product_id], material: params[:opened_material_id]) if params[:opened_product_id].present?
-
-    product_tracks       = current_spree_user.product_tracks.where(product_id: @products.map(&:id))
-
-    @opened_product_ids  = product_tracks.map(&:product_id)
-    @opened_material_ids = product_tracks.map(&:material).compact.map(&:self_and_ancestors).flatten.map(&:id).uniq
-    @material_ids_to_scroll = product_tracks.map(&:material_id).compact
+    set_product_tracks
   end
 
   private
@@ -26,5 +20,14 @@ class DownloadPagesController < ApplicationController
 
   def locked_product?(product)
     !product.free? && !@boughted_products.include?(product)
+  end
+
+  def set_product_tracks
+    current_spree_user.product_tracks.find_or_create_by(product_id: params[:opened_product_id], material: params[:opened_material_id]) if params[:opened_product_id].present?
+    product_tracks = current_spree_user.product_tracks.where(product_id: @products.map(&:id))
+
+    @opened_product_ids     = product_tracks.map(&:product_id)
+    @opened_material_ids    = product_tracks.map(&:material).compact.map(&:self_and_ancestors).flatten.map(&:id).uniq
+    @material_ids_to_scroll = product_tracks.map(&:material_id).compact
   end
 end
