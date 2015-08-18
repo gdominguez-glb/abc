3914dc950d171f6aa2b6306e81bee3fa4ea1b8d4
Roc Yu <rociiu.yu@gmail.com>
2015-08-18 16:56:33 +0800

    GM-65 implement LicenseReminderWorker, LicenseReminderMailer to send emails to user and licenses distributer when licenses is expiring in 15/30 days

---
+++ app/mailers/license_reminder_mailer.rb
@@ -0,0 +1,10 @@
+class LicenseReminderMailer < ApplicationMailer
+
+  def remind(attrs={})
+    @user              = attrs[:user]
+    @products          = attrs[:products]
+    @days              = attrs[:days]
+    @is_district_admin = attrs[:is_district_admin] || false
+    mail to: user.email, subject: "Your licenses are expiring"
+  end
+end
