7d313df7de6f0da92c8fc791e3b8eb53659fe6a2
Roc Yu <rociiu.yu@gmail.com>
2015-08-19 16:13:05 +0800

    GM-378 add taxons in videos admin form

--- app/controllers/spree/admin/videos_controller.rb
+++ app/controllers/spree/admin/videos_controller.rb
@@ -4,6 +4,16 @@ module Spree
       def index
         @videos = Spree::Video.page(params[:page])
       end
+
+      protected
+
+      def permitted_resource_params
+        _params = params.require(:video).permit!
+        if _params[:taxon_ids].present?
+          _params[:taxon_ids] = _params[:taxon_ids].split(',')
+        end
+        _params
+      end
     end
   end
 end
