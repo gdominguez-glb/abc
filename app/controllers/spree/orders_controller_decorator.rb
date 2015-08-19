56cc8ed95bd6784c8b917c7d5d13976934127cf7
Roc Yu <rociiu.yu@gmail.com>
2015-08-19 11:42:42 +0800

    GM-66 implement url to add product to cart via link

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -3,4 +3,13 @@ Spree::OrdersController.class_eval do
     @order = current_order || Spree::Order.incomplete.find_or_initialize_by(guest_token: cookies.signed[:guest_token])
     @order.contents.update_cart(order_params)
   end
+
+  def add_products_to_cart
+    order = current_order(create_order_if_necessary: true)
+    variants = Spree::Product.where(id: params[:product_ids]).includes(:master).map(&:master)
+    variants.each do |variant|
+      order.contents.add(variant, 1, {})
+    end
+    redirect_to spree.cart_path
+  end
 end
--- config/spree_routes.rb
+++ config/spree_routes.rb
@@ -56,6 +56,7 @@ spree_routes_overrides = Proc.new do
   end
   patch '/simple_cart', :to => 'orders#update_simple_cart', :as => :update_simple_cart
   post '/products/:id/favorite', to: 'products#favorite', as: :favorite_product
+  get '/add_products_to_cart', to: 'orders#add_products_to_cart', as: :add_products_to_cart
 end
 if Rails.env.development?
   Spree::Core::Engine.add_routes(&spree_routes_overrides)
