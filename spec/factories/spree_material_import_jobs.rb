81db751aa3ff6fe692bf205973764476c9526398
Roc Yu <rociiu.yu@gmail.com>
2015-08-24 16:51:26 +0800

    add test for Spree::MaterialImportJob

--- spec/factories/spree_material_import_jobs.rb
+++ spec/factories/spree_material_import_jobs.rb
@@ -1,8 +1,7 @@
 FactoryGirl.define do
-  factory :spree_material_import_job, :class => 'Spree::MaterialImportJob' do
-    product_id 1
+  factory :material_import_job, class: 'Spree::MaterialImportJob' do
     user_id 1
-    status "MyString"
+    product_id 1
   end
 
 end
