db85ea82f3e731de31bd6e1bca5cefa264f3e2fa
Roc Yu <rociiu.yu@gmail.com>
2015-08-24 16:44:32 +0800

    add test for Spree::Part model

--- app/models/spree/part.rb
+++ app/models/spree/part.rb
@@ -1,12 +1,7 @@
 module Spree
   class Part < ActiveRecord::Base
-    belongs_to :bundle,
-               class_name: 'Spree::Product',
-               foreign_key: 'bundle_id'
-
-    belongs_to :product,
-               class_name: 'Spree::Product',
-               foreign_key: 'product_id'
+    belongs_to :bundle, class_name: 'Spree::Product', foreign_key: 'bundle_id'
+    belongs_to :product, class_name: 'Spree::Product', foreign_key: 'product_id'
 
     after_create :remove_can_be_part_flag
 
