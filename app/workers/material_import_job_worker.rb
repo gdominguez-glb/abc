834c47bc872b70ca1fe8ca1befdb10e9789d73f7
Roc Yu <rociiu.yu@gmail.com>
2015-08-24 13:21:09 +0800

    refactor MaterialImportJobWorker to improve code climate score

--- app/workers/material_import_job_worker.rb
+++ app/workers/material_import_job_worker.rb
@@ -12,41 +12,52 @@ class MaterialImportJobWorker
     material_import_job.update(status: 'processing')
 
     begin
-      tmp_directory_path  = Rails.root.join('tmp/material_import_jobs', material_import_job_id.to_s)
-      FileUtils.mkdir_p(tmp_directory_path)
+      FileUtils.mkdir_p(tmp_directory_path(material_import_job))
 
-      zip_file_path = File.join(tmp_directory_path, material_import_job.file.original_filename)
-      material_import_job.file.copy_to_local_file(:original, zip_file_path)
-
-      raw_file_directory = File.join(tmp_directory_path, 'raw')
-      FileUtils.mkdir_p(raw_file_directory)
-
-      Zip::File.open(zip_file_path) do |zip_file|
-        zip_file.each do |entry|
-          file_path= File.join(raw_file_directory, entry.name)
-          FileUtils.mkdir_p(File.dirname(file_path))
-          zip_file.extract(entry, file_path) unless File.exist?(file_path)
-          puts "Extracting #{entry.name}"
-        end
-      end
-
-      product = material_import_job.product
-      root_directory_path = File.join(raw_file_directory, product.name)
-
-      if File.exists?(root_directory_path)
-        dir = Dir.new(root_directory_path)
-        dir.entries.each do |sub_dir_name|
-          sub_dir_path = File.join(root_directory_path, sub_dir_name)
-          next if sub_dir_name.start_with?('.')
-          next if !File.directory?(sub_dir_path)
-          process_directory(product, nil, sub_dir_path)
-        end
-      end
+      material_import_job.file.copy_to_local_file(:original, zip_file_path(material_import_job))
+      extract_zip_file(zip_file_path(material_import_job), raw_file_directory_path(material_import_job))
+      process_root_directory(material_import_job)
 
       material_import_job.update(status: 'done')
     rescue => exception
       material_import_job.update(status: 'failed')
       ExceptionNotifier.notify_exception(exception, :data => {:message => "Fail to process import job #{material_import_job_id}"})
+    end
+  end
+
+  def tmp_directory_path(material_import_job)
+    Rails.root.join('tmp/material_import_jobs', material_import_job.id.to_s)
+  end
+
+  def zip_file_path(material_import_job)
+    File.join(tmp_directory_path(material_import_job), material_import_job.file.original_filename)
+  end
+
+  def raw_file_directory_path(material_import_job)
+    File.join(tmp_directory_path(material_import_job), 'raw')
+  end
+
+  def extract_zip_file(zip_file_path, dest_directory)
+    Zip::File.open(zip_file_path) do |zip_file|
+      zip_file.each do |entry|
+        file_path= File.join(dest_directory, entry.name)
+        FileUtils.mkdir_p(File.dirname(file_path))
+        zip_file.extract(entry, file_path) unless File.exist?(file_path)
+        puts "Extracting #{entry.name}"
+      end
+    end
+  end
+
+  def process_root_directory(material_import_job)
+    product             = material_import_job.product
+    root_directory_path = File.join(raw_file_directory_path(material_import_job), product.name)
+    return unless File.exists?(root_directory_path)
+    dir = Dir.new(root_directory_path)
+    dir.entries.each do |sub_dir_name|
+      sub_dir_path = File.join(root_directory_path, sub_dir_name)
+      next if sub_dir_name.start_with?('.')
+      next if !File.directory?(sub_dir_path)
+      process_directory(product, nil, sub_dir_path)
     end
   end
 
