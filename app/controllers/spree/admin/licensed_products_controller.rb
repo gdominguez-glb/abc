d463fa854a28071f62fc14662a874106f1aaea64
Roc Yu <rociiu.yu@gmail.com>
2015-08-25 09:43:37 +0800

    fix exception in licenses products list admin when product no longer exist

--- app/controllers/spree/admin/licensed_products_controller.rb
+++ app/controllers/spree/admin/licensed_products_controller.rb
@@ -2,7 +2,7 @@ module Spree
   module Admin
     class LicensedProductsController < ResourceController
       def index
-        @licensed_products = Spree::LicensedProduct.includes([:user, :product]).page(params[:page])
+        @licensed_products = Spree::LicensedProduct.joins(:product).includes([:user, :product]).page(params[:page])
       end
 
       def new
