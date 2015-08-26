7ba6706bd3a0e9760151421516bcc20205e6fbbb
Roc Yu <rociiu.yu@gmail.com>
2015-08-26 21:41:07 +0800

    refactor LicenseReminderWorker to improve codeclimate score

--- app/workers/license_reminder_worker.rb
+++ app/workers/license_reminder_worker.rb
@@ -9,25 +9,37 @@ class LicenseReminderWorker
   end
 
   def notify_user(days)
-    Spree::LicensedProduct.
-      where("date(expire_at) = ?", days.days.since.to_date).
-      group('user_id, product_id').select(:user_id, :product_id).
-      group_by(&:user_id).each do |user_id, licensed_products|
-        user = Spree::User.find(user_id)
-        LicenseReminderMailer.remind({ user: user, products: licensed_products.map(&:product).uniq, days: 30 })
+    licenses_group = expiring_licenses_group_in_days(days)
+    licenses_group.each do |user_id, licensed_products|
+      LicenseReminderMailer.remind({ user: Spree::User.find(user_id), products: licensed_products.map(&:product).uniq, days: days })
     end
   end
 
+  def expiring_licenses_group_in_days(days)
+    Spree::LicensedProduct.
+      expire_in_days(days).
+      group('user_id, product_id').select(:user_id, :product_id).
+      group_by(&:user_id)
+  end
+
   def notify_distributer(days)
-    distribution_ids = Spree::LicensedProduct.
-      where("date(expire_at) = ?", days.days.since.to_date).
-      where.not(product_distribution_id: nil).pluck(:product_distribution_id)
+    distribution_ids    = expiring_distribution_ids(days)
+    distributions_group = user_distributions_group(distribution_ids)
+    distributions_group.each do |from_user_id, distributions|
+      LicenseReminderMailer.remind({ user: Spree::User.find(user_id), products: distributions.map(&:product).uniq, days: days, is_district_admin: true })
+    end
+  end
+
+  def user_distributions_group(distribution_ids)
     Spree::ProductDistribution.
       where(id: distribution_ids).
       select(:from_user_id, :product_id).
-      group(:from_user_id, :product_id).group_by(&:from_user_id).each do |from_user_id, distributions|
-        user = Spree::User.find(user_id)
-        LicenseReminderMailer.remind({ user: user, products: distributions.map(&:product).uniq, days: 30, is_district_admin: true })
-    end
+      group(:from_user_id, :product_id).group_by(&:from_user_id)
+  end
+
+  def expiring_distribution_ids(days)
+    Spree::LicensedProduct.
+      expire_in_days(days).
+      where.not(product_distribution_id: nil).pluck(:product_distribution_id)
   end
 end
