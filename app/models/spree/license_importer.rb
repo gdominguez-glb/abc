582d9f9bd475cbae2290906ecf7f0a8b8ba5bf41
Roc Yu <rociiu.yu@gmail.com>
2015-08-26 19:22:01 +0800

    GM-400 update licenses importer in admin to use product name to refer product instead of id, add specify error message on licenses importer

--- app/models/spree/license_importer.rb
+++ app/models/spree/license_importer.rb
@@ -7,22 +7,28 @@ class Spree::LicenseImporter
     xlsx   = Roo::Excelx.new(@file.path)
     sheet  = xlsx.sheet(0)
     header = sheet.row(1)
-    return false if !valid_header?(header)
+    return { error: 'Invalid excel header' } if !valid_header?(header)
 
     licensed_products = construct_records(header, sheet)
-    return false if !licensed_products.all?{|lp| lp.valid? }
+    return { error: 'Invalid product/quantity' } if !licensed_products.all?{|lp| lp.valid? }
 
     licensed_products.each{|lp| lp.save }
+    return {}
   end
 
   def valid_header?(header)
-    header.map(&:downcase) == ['email', 'product_id', 'quantity']
+    header.map(&:downcase) == ['email', 'product', 'quantity']
   end
 
   def construct_records(header, sheet)
     (2..sheet.last_row).map do |i|
-      row = Hash[[header, sheet.row(i)].transpose]
-      Spree::LicensedProduct.new(row)
+      row = Hash[[header.map(&:downcase), sheet.row(i)].transpose]
+      product = Spree::Product.find_by name: row['product']
+      Spree::LicensedProduct.new(
+        email: row['email'],
+        product: product,
+        quantity: row['quantity']
+      )
     end
   end
 end
