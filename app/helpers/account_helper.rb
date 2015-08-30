88c44ed21285aa09c1eda2480ec905263843ed2d
Roc Yu <rociiu.yu@gmail.com>
2015-08-31 07:17:00 +0800

    refactor Account::Helper to improve codeclimate score

--- app/helpers/account_helper.rb
+++ app/helpers/account_helper.rb
@@ -1,17 +1,31 @@
 module AccountHelper
   def activity_item_link(activity)
     return '' if activity.item.blank?
-    if activity.item_type == 'Page'
-      "/#{activity.item.slug}"
-    elsif activity.item_type == 'Spree::Product'
-      if activity.action == 'download'
-        "#{activity.item.access_url}?#{{opened_product_id: activity.item.id}.to_param}"
-      else
-        spree.product_path(activity.item.slug)
-      end
-    elsif activity.item_type == 'Spree::Material'
-      product = activity.item.product
-      "#{product.access_url}?#{{opened_product_id: product.id, opened_material_id: activity.item.id}.to_param}"
+
+    case activity.item_type
+    when 'Page'
+      generate_page_activity_url(activity)
+    when 'Spree::Product'
+      generate_product_activity_url(activity)
+    when 'Spree::Material'
+      generate_material_activity_url(activity)
     end
+  end
+
+  def generate_page_activity_url(activity)
+    "/#{activity.item.slug}"
+  end
+
+  def generate_product_activity_url(activity)
+    if activity.action == 'download'
+      "#{activity.item.access_url}?#{{opened_product_id: activity.item.id}.to_param}"
+    else
+      spree.product_path(activity.item.slug)
+    end
+  end
+
+  def generate_material_activity_url(activity)
+    product = activity.item.product
+    "#{product.access_url}?#{{opened_product_id: product.id, opened_material_id: activity.item.id}.to_param}"
   end
 end
