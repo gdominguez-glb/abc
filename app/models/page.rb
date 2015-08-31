da2459b8ce08c69fa93cfdf765634739dc4cc5d9
Roc Yu <rociiu.yu@gmail.com>
2015-08-31 16:25:40 +0800

    GM-35 add definitions for different tiles name/fields

--- app/models/page.rb
+++ app/models/page.rb
@@ -21,4 +21,45 @@ class Page < ActiveRecord::Base
   scope :show_in_footer_as_top_links, -> { visibles.group_roots.where(show_in_footer: true) }
   scope :show_in_footer_as_subgroup_links, -> (group_name) { visibles.not_group_roots.where(show_in_footer: true, group_name: group_name) }
 
+  TILES = [
+    {
+      name: 'Masthead',
+      fields: [
+        { label: 'Title', input_type: 'text' },
+        { label: 'Background Image', input_type: 'text' }
+      ]
+    },
+    {
+      name: '50/50 Small Image',
+      fields: [
+        { label: 'Title', input_type: 'text' },
+        { label: 'Description', input_type: 'text_area' },
+        { label: 'Lean More Url', input_type: 'text' },
+        { label: 'Image', input_type: 'text' }
+      ] },
+    {
+      name: 'Three Columns',
+      fields: [
+        { label: 'Title 1', input_type: 'text' },
+        { label: 'Description 1', input_type: 'text_area' },
+        { label: 'Lean More Url 1', input_type: 'text' },
+        { label: 'Title 2', input_type: 'text' },
+        { label: 'Description 2', input_type: 'text_area' },
+        { label: 'Lean More Url 2', input_type: 'text' },
+        { label: 'Title 3', input_type: 'text' },
+        { label: 'Description 3', input_type: 'text_area' },
+        { label: 'Lean More Url 3', input_type: 'text' }
+      ]
+    },
+    {
+      name: '50/50 Large Image',
+      fields: [
+        { label: 'Title', input_type: 'text' },
+        { label: 'Description', input_type: 'text_area' },
+        { label: 'Lean More Url', input_type: 'text' },
+        { label: 'Download Url', input_type: 'text' },
+        { label: 'Image', input_type: 'text' }
+      ]
+    }
+  ]
 end
