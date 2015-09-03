b396d573a4ba64031427896be481dbcfac5b6184
Jeff Pelletier <jeff.pelletier@intridea.com>
2015-09-03 14:19:57 -0400

    Radio buttons always visible with Teacher, Admin, Admin Asst.

--- app/assets/javascripts/user_form.js
+++ app/assets/javascripts/user_form.js
@@ -11,20 +11,16 @@ $(document).on("change", ".select-container select", function(){
 function shouldShowDistrict(userRole) {
   var visibleFields = {};
   visibleFields.showDistrictDetails = false;
-  visibleFields.showRadio = false;
 
   switch(userRole) {
     case "Teacher":
       visibleFields.showDistrictDetails = true;
-      visibleFields.showRadio = true;
       break;
     case "Administrator":
       visibleFields.showDistrictDetails = true;
-      visibleFields.showRadio = true;
       break;
     case "Administrative Assistant":
       visibleFields.showDistrictDetails = true;
-      visibleFields.showRadio = true;
       break;
     case "Parent":
       console.log("Hi mom");
@@ -54,12 +50,6 @@ $(document).on("change", "#spree_user_title", function(){
     $('.school-district-details').collapse('show');
   } else {
     $('.school-district-details').collapse('hide');
-  }
-
-  if(showVisibleFields.showRadio) {
-    $('#schoolDistrictSelect').collapse('show');
-  } else {
-    $('#schoolDistrictSelect').collapse('hide')
   }
 });
 
