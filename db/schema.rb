b40ad63eeb0daa4cdb43e50b40f30254eb27b0e8
Roc Yu <rociiu.yu@gmail.com>
2015-09-08 09:28:51 +0800

    GM-516 add for_sale column to products, add for_sale inputs in product admin form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150907124718) do
+ActiveRecord::Schema.define(version: 20150908002539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -893,6 +893,7 @@ ActiveRecord::Schema.define(version: 20150907124718) do
     t.boolean  "individual_sale",      default: true,  null: false
     t.integer  "video_group_id"
     t.datetime "fulfillment_date"
+    t.boolean  "for_sale",             default: false
   end
 
   add_index "spree_products", ["available_on"], name: "index_spree_products_on_available_on", using: :btree
