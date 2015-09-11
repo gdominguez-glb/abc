d82cd4d1dd31e3c26bae9b46e1cc24dca66b47ab
Roc Yu <rociiu.yu@gmail.com>
2015-09-12 07:15:52 +0800

    add SingleLicenseExtractor to distribute one license to school admin himself

--- app/models/spree/licenses_manager/single_license_extractor.rb
+++ app/models/spree/licenses_manager/single_license_extractor.rb
@@ -6,7 +6,45 @@ module Spree
       end
 
       def execute
-        @licensed_product.distribute_one_license_to_self
+        return if exist_product_license_not_expire?
+        Spree::LicensedProduct.transaction do
+          create_new_license(construct_distribution)
+          update_original_license
+        end
+      end
+
+      def exist_product_license_not_expire?
+        @licensed_product.user.
+          licensed_products.
+          where(can_be_distributed: false, product_id: @licensed_product.product_id).
+          where("id != ?", @licensed_product.id).
+          where("expire_at > ?", Time.now).
+          exists?
+      end
+
+      def construct_new_license(distribution)
+        single_licensed_product = @licensed_product.dup
+        single_licensed_product.update(quanity: 1, product_distribution_id: distribution.id)
+      end
+
+      def construct_distribution
+        user_id    = @licensed_product.user.try(:id)
+        user_email = @licensed_product.email
+
+        Spree::ProductDistribution.create(
+          licensed_product:    @licensed_product,
+          from_user_id:        user_id,
+          from_email:          user_email,
+          to_user_id:          user_id,
+          email:               user_email,
+          quanity:             1,
+          product_id:          @licensed_product.product_id
+          skip_create_license: true
+        )
+      end
+
+      def update_original_license
+        @licensed_product.update(quanity: @licensed_product - 1)
       end
     end
   end
