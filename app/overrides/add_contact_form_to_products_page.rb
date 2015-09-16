6151ba5c1dfb291565659ffc50be454b231edaf8
Jeff Pelletier <jeff.pelletier@intridea.com>
2015-09-16 17:02:16 -0400

    Sidebar only appears on products page.

--- app/overrides/add_contact_form_to_products_page.rb
+++ app/overrides/add_contact_form_to_products_page.rb
@@ -1,13 +1,13 @@
 Deface::Override.new(
     virtual_path: "spree/shared/_products",
     name: "add_contact_form_banner_to_products_page",
-    insert_before: "div#products",
+    insert_before: "div#sidebar",
     text: '
-    <div class="call-banner blue-bg">
-      <h5>Bulk Order? Mixed Order?</h5>
-      <h6>Our experienced representatives are standing by to help you with your special order.
+    <div class="page-header">
+      <h2>Bulk Order? Mixed Order?</h2>
+      <p>Our experienced representatives are standing by to help you with your special order.
         <a href="" data-toggle="modal" data-target="#productContactForm">Contact us</a> today
-      <h6>
+      </p>
     </div>'
 )
 
