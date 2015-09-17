d17b551f69ab78ca86cab9ff54d365264c1900e2
Jeff Pelletier <jeff.pelletier@intridea.com>
2015-09-17 10:28:33 -0400

    Redirect users when they click Go.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -6,14 +6,30 @@ $(document).ready(function() {
 	var dropdownCurriculum = pageSelection.find('#dropdownCurriculum');
 	var btnGo = pageSelection.find('#btnGo');
 
-	dropdownLinks.on('click', function() {
+	dropdownLinks.on('click', function(e) {
 		var text = $(this).text();
 		$(this).addClass('selected');
 		$(this).closest('.btn-group').find('.btn').html(text + ' <span class="caret"></span>');
 
+		e.preventDefault();
+
 		if (dropdownRole.next().find('.selected')[0] && dropdownCurriculum.next().find('.selected')[0]) {
 			btnGo.removeAttr('disabled');
 		}
+	});
+
+	btnGo.on('click', function(e) {
+		e.preventDefault();
+
+		var roleText = dropdownRole.text().trim().toLowerCase() + 's';
+		var curriculumText = dropdownCurriculum.text().trim().toLowerCase();
+
+		if (roleText === 'administrators') {
+			roleText = 'admins';
+		}
+
+		var url = curriculumText + '/' + roleText;
+		window.location = url;
 	});
 
 });
