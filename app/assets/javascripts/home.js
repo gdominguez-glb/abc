18bf6d83e50eb1f351af0582b64012de5ccc517a
Jeff Pelletier <jeff.pelletier@intridea.com>
2015-09-17 10:14:45 -0400

    Enable Go button when role and curriculum are selected.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,10 +2,18 @@ $(document).ready(function() {
 
 	var pageSelection = $('#pageSelection');
 	var dropdownLinks = pageSelection.find('.dropdown-menu a');
+	var dropdownRole = pageSelection.find('#dropdownRole');
+	var dropdownCurriculum = pageSelection.find('#dropdownCurriculum');
+	var btnGo = pageSelection.find('#btnGo');
 
 	dropdownLinks.on('click', function() {
 		var text = $(this).text();
+		$(this).addClass('selected');
 		$(this).closest('.btn-group').find('.btn').html(text + ' <span class="caret"></span>');
+
+		if (dropdownRole.next().find('.selected')[0] && dropdownCurriculum.next().find('.selected')[0]) {
+			btnGo.removeAttr('disabled');
+		}
 	});
 
 });
