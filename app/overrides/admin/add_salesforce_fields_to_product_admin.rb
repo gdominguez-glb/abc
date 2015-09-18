526577a8bb8bc21a6f6da10079422b35ef1a889a
Adam Frazin <adam@mobomo.com>
2015-09-18 09:36:52 -0400

    Add "For Sale" message and correct HTML

--- app/overrides/admin/add_salesforce_fields_to_product_admin.rb
+++ app/overrides/admin/add_salesforce_fields_to_product_admin.rb
@@ -1,17 +1,17 @@
 Deface::Override.new(
     virtual_path: "spree/admin/products/_form",
     name: "add_salesforce_fields_to_product_admin",
-    insert_bottom: "[data-hook='admin_product_form_additional_fields']",
+    insert_after: "[data-hook='admin_product_form_for_sale']",
     text: "
           <div data-hook='admin_product_form_sf_id_product'>
             <div class='form-group field' id='product_sf_id_product_field'>
-              <%= f.label :sf_id_product %>
+              <%= f.label :sf_id_product %><br /><%= t(:salesforce_field_note) %>
               <%= f.text_field :sf_id_product, class: 'form-control' %>
             </div>
           </div>
           <div data-hook='admin_product_form_sf_id_pricebook'>
             <div class='form-group field' id='product_sf_id_pricebook_field'>
-              <%= f.label :sf_id_pricebook %>
+              <%= f.label :sf_id_pricebook %><br /><%= t(:salesforce_field_note) %>
               <%= f.text_field :sf_id_pricebook, class: 'form-control' %>
             </div>
           </div>
@@ -21,17 +21,17 @@ Deface::Override.new(
 Deface::Override.new(
     virtual_path: "spree/admin/products/new",
     name: "add_salesforce_fields_to_product_new",
-    insert_bottom: "[data-hook='new_product_attrs']",
+    insert_after: "[data-hook='new_product_for_sale']",
     text: "
           <div data-hook='new_product_sf_id_product' class='col-md-4'>
             <div class='form-group field' id='product_sf_id_product_field'>
-              <%= f.label :sf_id_product %>
+              <%= f.label :sf_id_product %><br /><%= t(:salesforce_field_note) %>
               <%= f.text_field :sf_id_product, class: 'form-control' %>
             </div>
           </div>
           <div data-hook='admin_product_form_license_length' class='col-md-4'>
             <div class='form-group field' id='product_sf_id_pricebook_field'>
-              <%= f.label :sf_id_pricebook %>
+              <%= f.label :sf_id_pricebook %><br /><%= t(:salesforce_field_note) %>
               <%= f.text_field :sf_id_pricebook, class: 'form-control' %>
             </div>
           </div>
