2954c1ab9dc8a4e83e00eff4e6099cb8c1f1ff0c
Roc Yu <rociiu.yu@gmail.com>
2015-09-21 14:51:03 +0800

    fix scraper test

--- spec/lib/medium/scraper_spec.rb
+++ spec/lib/medium/scraper_spec.rb
@@ -4,6 +4,8 @@ require 'medium'
 RSpec.describe Medium::Scraper do
   let(:post_data) do
     {
+      'id' => 'abc123',
+      'slug' => 'abc123',
       'success' => true,
       'payload' => {
         'value' => {
@@ -44,12 +46,16 @@ RSpec.describe Medium::Scraper do
   before(:each) do
     stub_request(:get, "http://medium.com/abc123/latest?format=json").
       with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
-      to_return(:status => 200, :body => {'payload' => { 'posts' => [ post_data ] } }.to_json, :headers => {})
+      to_return(:status => 200, :body => ("])}while(1);</x>" + {'payload' => { 'posts' => [ post_data ] } }.to_json), :headers => {})
     
+    stub_request(:get, "http://medium.com/abc123/abc123-abc123?format=json").
+      with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+      to_return(:status => 200, :body => ("])}while(1);</x>" + post_data.to_json), :headers => {})
+
     Medium::Scraper.new.scrape_publications
   end
 
   it "import posts from medium" do
-    expect(MediumPublication.count).to eq(1)
+    expect(Post.count).to eq(1)
   end
 end
