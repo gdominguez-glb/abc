92effbe1c250195156cc1abaeddd011424731e9b
Roc Yu <rociiu.yu@gmail.com>
2015-09-21 08:57:15 +0800

    cleanup digital related method in Product model

--- spec/models/spree/product_spec.rb
+++ spec/models/spree/product_spec.rb
@@ -16,24 +16,27 @@ RSpec.describe Spree::Product, type: :model do
     end
   end
 
-  describe "#downloadable?" do
-    let(:pdf_product) { create(:product, product_type: 'Pdf') }
-    let(:video_product) { create(:product, product_type: 'Video') }
-
-    it "return true for pdf product" do
-      expect(pdf_product.downloadable?).to eq(true)
-    end
-
-    it "return false for video product" do
-      expect(video_product.downloadable?).to eq(false)
-    end
-  end
-
   describe "assign video group taxon" do
     it "assign taxon with video group name" do
       video_group = create(:spree_video_group, name: 'Teach Eureka')
       product = create(:product, video_group: video_group)
       expect(product.taxons.find_by(name: 'Teach Eureka')).not_to be_nil
+    end
+  end
+
+  describe "#digital?" do
+    let(:product) { create(:product) }
+
+    it "return true if product type is digital" do
+      product.product_type = 'Digital'
+
+      expect(product.digital?).to eq(true)
+    end
+
+    it "return false if product type is not digital" do
+      product.product_type = 'Pdf'
+      
+      expect(product.digital?).to eq(false)
     end
   end
 end
