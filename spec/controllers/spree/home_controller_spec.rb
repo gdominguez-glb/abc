f7895dce68daa45fb94dca42736efc783e1dca81
Roc Yu <rociiu.yu@gmail.com>
2015-09-21 16:39:12 +0800

    add test for Spree::HomeController decorator

---
+++ spec/controllers/spree/home_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Spree::HomeController, type: :controller do
+  routes { Spree::Core::Engine.routes }
+
+  describe "GET 'index'" do
+    let!(:product) { create(:product, individual_sale: true, for_sale: true, available_on: 1.days.ago) }
+    let!(:store_taxonomy) { create(:taxonomy, show_in_store: true, show_in_video: false) }
+    let!(:video_taxonomy) { create(:taxonomy, show_in_store: false, show_in_video: true) }
+
+    before(:each) do
+      get :index
+    end
+
+    it "set products for sale" do
+      expect(assigns(:products)).to eq([product])
+    end
+
+    it "set store taxonomies" do
+      expect(assigns(:taxonomies)).to eq([store_taxonomy])
+    end
+  end
+end
