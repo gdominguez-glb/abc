73c002b92f5056611a180225d8ce70c7f9e8e60c
Roc Yu <rociiu.yu@gmail.com>
2015-09-22 10:52:17 +0800

    GM-586 implement new taxonomy/taxons sidebar filter in store to match new comp

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
     return '' if request.fullpath == '/store/login'
     'active'
   end
+
+  def store_generate_taxon_ids_param(taxon_ids, sibling_ids, taxon)
+    selected_taxon_id = (taxon_ids & sibling_ids).first
+    if selected_taxon_id
+      taxon_ids.delete(selected_taxon_id)
+    end
+    taxon_ids << taxon.id
+  end
 end
