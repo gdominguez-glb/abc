9753cb7c53d4746f5f4758dfcc5f604fe9f80c97
Roc Yu <rociiu.yu@gmail.com>
2015-09-30 15:12:00 +0800

    extract AutocompleteByName concern for duplicate code

---
+++ app/controllers/concerns/autocomplete_by_name.rb
@@ -0,0 +1,22 @@
+module AutocompleteByName
+  extend ActiveSupport::Concern
+
+  def autocomplate_by_name(klass, id_name=:id)
+    collection = klass.page(params[:page])
+
+    collection_name = klass.name.split('::').last.underscore.pluralize
+
+    if params[:q]
+      collection = collection.where("name like ?", "%#{params[:q]}%")
+    end
+
+    render json: {
+      count: collection.count,
+      total_count: collection.total_count,
+      current_page: (params[:page] ? params[:page].to_i : 1),
+      per_page: (params[:per_page] || Kaminari.config.default_per_page),
+      pages: collection.num_pages,
+      collection_name => collection.map{|item| { id: item.send(id_name), name: item.name }}
+    }
+  end
+end
--- app/controllers/spree/admin/school_districts_controller.rb
+++ app/controllers/spree/admin/school_districts_controller.rb
@@ -1,26 +1,10 @@
 module Spree
   module Admin
     class SchoolDistrictsController < Spree::Admin::BaseController
+      include AutocompleteByName
 
       def index
-        @school_districts = SchoolDistrict.page(params[:page])
-
-        if params[:q]
-          @school_districts = @school_districts.where("name like ?", "%#{params[:q]}%")
-        end
-
-        if params[:ids].present?
-          @school_districts = @school_districts.where(id: params[:ids])
-        end
-
-        render json: {
-          count: @school_districts.count,
-          total_count: @school_districts.total_count,
-          current_page: (params[:page] ? params[:page].to_i : 1),
-          per_page: (params[:per_page] || Kaminari.config.default_per_page),
-          pages: @school_districts.num_pages,
-          school_districts: @school_districts.map{|vg| { id: vg.id, name: vg.name }}
-        }
+        autocomplate_by_name(SchoolDistrict)
       end
 
     end
