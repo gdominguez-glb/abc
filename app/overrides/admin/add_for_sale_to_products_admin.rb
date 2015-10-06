9685f75bedbe9fae846869228cd984affae55e31
Roc Yu <rociiu.yu@gmail.com>
2015-10-06 19:46:57 +0800

    GM-642 adjust layout in product detail page to fill the blank, remove extra fields

--- app/overrides/admin/add_for_sale_to_products_admin.rb
+++ app/overrides/admin/add_for_sale_to_products_admin.rb
@@ -1,18 +1,4 @@
 Deface::Override.new(
-    virtual_path: "spree/admin/products/_form",
-    name: "add_for_sale_to_product_admin",
-    insert_after: "[data-hook='admin_product_form_available_on']",
-    text: "
-          <div data-hook='admin_product_form_for_sale'>
-            <div class='form-group field' id='product_for_sale_field'>
-              <%= f.label :for_sale %>
-              <%= f.check_box :for_sale, class: 'form-control' %>
-            </div>
-          </div>
-"
-)
-
-Deface::Override.new(
     virtual_path: "spree/admin/products/new",
     name: "add_for_sale_to_product_new",
     insert_after: "[data-hook='new_product_available_on']",
