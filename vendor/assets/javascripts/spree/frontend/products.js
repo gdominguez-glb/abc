7845308bfbde9d2e43b3204e1a62bd2fc4058c84
Jeff Pelletier <jeff.pelletier@intridea.com>
2015-10-08 16:29:08 -0400

    Price display is consistent.

--- vendor/assets/javascripts/spree/frontend/products.js
+++ vendor/assets/javascripts/spree/frontend/products.js
@@ -1,13 +1,22 @@
 //Products JavaScript
 
 $(document).ready(function() {
-	var prices = $('.price a');
+	var storePrices = $('#products .price a');
+	var individualProductPrice = $('#product-details .price');
 
-	prices.each(function() {
-		if($(this).text() === "$0.00") {
-			$(this).text('Free');
-		}
-	});
+	function setFree(prices) {
+		prices.each(function() {
+			if($(this).text().trim() === "$0.00") {
+				$(this).text('Free');
+			}
+		});
+	}
+
+	if (storePrices[0]) {
+		setFree(storePrices);
+	} else if (individualProductPrice[0]) {
+		setFree(individualProductPrice);
+	}
 
 	var flipBtn = $('.flip-btn');
   flipBtn.on('click', function() {
