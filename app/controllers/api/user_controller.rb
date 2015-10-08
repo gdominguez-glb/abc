cb933ab6d63293017477f9e7a2b392b93b86cbc8
Roc Yu <rociiu.yu@gmail.com>
2015-10-08 15:03:11 +0800

    GM-518 update /api/user/info api to include products that user have access

--- app/controllers/api/user_controller.rb
+++ app/controllers/api/user_controller.rb
@@ -1,6 +1,17 @@
 class Api::UserController < Api::BaseController
   def info
     user = Spree::User.find(doorkeeper_token.resource_owner_id)
-    render json: { name: user.full_name, permissions: [] }
+    render json: {
+      name: user.full_name,
+      products: products_of_user(user).as_json(only: [:id, :name])
+    }
+  end
+
+  private
+
+  def products_of_user(user)
+    user.products.map do |product|
+      product.parts.exists? ? product.parts : product
+    end.flatten.compact.uniq
   end
 end
