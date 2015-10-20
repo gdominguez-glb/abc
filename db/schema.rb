9d96edb7c297653173c97b03183d2608aebfb6bf
Roc Yu <rociiu.yu@gmail.com>
2015-10-20 14:43:29 +0800

    GM-722 add preview_image_url to Spree::Video extract preview image from wistia video assets

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016094559) do
+ActiveRecord::Schema.define(version: 20151020063322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1578,6 +1578,7 @@ ActiveRecord::Schema.define(version: 20151016094559) do
     t.string   "wistia_status"
     t.string   "wistia_thumbnail_url"
     t.integer  "video_group_id"
+    t.string   "preview_image_url"
   end
 
   create_table "spree_zone_members", force: :cascade do |t|
