d459a18006c7e93b2230d0dcf1833aaa0a56d990
Roc Yu <rociiu.yu@gmail.com>
2015-10-22 08:26:45 +0800

    GM-757 fixed issue that some of license not show up in users admin section due to fulfillment filter

--- app/controllers/spree/admin/users_controller_decorator.rb
+++ app/controllers/spree/admin/users_controller_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Admin::UsersController.class_eval do
   def licenses
     @user = Spree::User.find(params[:id])
-    @licensed_products = @user.licensed_products.includes(:product).page(params[:page])
+    @licensed_products = Spree::LicensedProduct.where(user_id: @user.id).includes(:product).page(params[:page])
   end
 end
