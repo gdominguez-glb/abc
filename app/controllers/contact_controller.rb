353cdfe0e65504f36d764930669d8dc4397a191c
Roc Yu <rociiu.yu@gmail.com>
2015-10-23 21:25:05 +0800

    GM-675 create salesforce objects from contact submission

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,12 +1,14 @@
 class ContactController < ApplicationController
 
   def index
-    @contact_form = ContactForm.new
+    @contact_form = ContactForm.new(school_district_type: 'School')
   end
 
   def create
     @contact_form = ContactForm.new(contact_form_params)
     if @contact_form.valid?
+      @contact_form.perform
+      flash[:notice] = "Successfully submited contact info."
       redirect_to contact_path
     else
       render :index
@@ -16,6 +18,8 @@ class ContactController < ApplicationController
   private
 
   def contact_form_params
-    params.require(:contact_form).permit!
+    params.require(:contact_form).permit(:topic, :support_type, :first_name, :last_name, :email, :phone, :role, :school_district_name, :school_district_type,
+    :country, :state, :curriculum, :grade, :school_district_size, :title_1, :returning_customer, :tax_exempt, :tax_exempt_id, :desired_dates,
+    :desired_training_topic, :items_purchased, :format, :description, :school_district)
   end
 end
--- app/models/contact_form.rb
+++ app/models/contact_form.rb
@@ -12,6 +12,54 @@ class ContactForm
   validates_presence_of :description, if: :require_description?
   validates_presence_of :format, if: ->{ self.support_type == 'Content Error' }
 
+  def perform
+    if ['Print or Bulk Orders', 'Sales and Purchasing', 'Professional Development'].include?(self.topic)
+      create_lead_object
+    elsif ['General', 'Support'].include?(self.topic)
+      create_case_object
+    end
+  end
+
+  def create_lead_object
+    GmSalesforce::Client.instance.create('Lead', {
+      'FirstName' => self.first_name,
+      'LastName'  => self.last_name,
+      'Title'     => self.role,
+      'Email'     => self.email,
+      'Phone'     => self.phone,
+      'Title_1__c' => self.title_1,
+      'What_curriculum_are_you_interested_in__c' => self.curriculum,
+      # 'Type__c'   => 'PD Request',
+      'Country'   => self.country,
+      'State'     => self.state,
+      'Description'  => self.description,
+
+      'School_or_District__c' => self.school_district_type,
+      'Company' => self.school_district_name,
+      'Returning_Customer__c' => self.returning_customer
+      # 'Lead_Source' => 'Web-Sales'
+    })
+  end
+
+  def create_case_object
+    GmSalesforce::Client.instance.create('Case', {
+      'First_Name__c' => self.first_name,
+      'Last_Name__c'  => self.last_name,
+      'Role__c'     => self.role,
+      'Email__c'     => self.email,
+      'Phone__c'     => self.phone,
+      'Description' => self.description,
+      'School_District__c' => self.school_district_type,
+      'School_District_Name__c' => self.school_district_name,
+      'Items_Purchased__c' => self.items_purchased,
+      'What_curriculum_are_you_interested_in__c' => self.curriculum,
+      'Format__c' => self.format,
+      'Origin' => 'Web'
+    })
+  end
+
+  private
+
   def require_description?
     ['General', 'Professional Development'].include?(self.topic) || 
       ( self.topic == 'Support' && ['Order Support', 'Parent Support', 'Content/Implementation Support', 'Technical Support'].include?(self.support_type) )
