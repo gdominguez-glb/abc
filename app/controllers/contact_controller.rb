29b58024593ce34da33a97a225f2c394fea4a626
Roc Yu <rociiu.yu@gmail.com>
2015-10-23 19:19:12 +0800

    GM-675 add validation for contact form in backend

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -2,8 +2,20 @@ class ContactController < ApplicationController
 
   def index
     @contact_form = ContactForm.new
+  end
 
-    @topics = ["General", "Print or Bulk Orders", "Sales and Purchasing", "Professional Development", "Support"]
-    @supports = ["Order Support", "Parent Support", "Content/Implementation Support", "Content Error", "Technical Support"]
+  def create
+    @contact_form = ContactForm.new(contact_form_params)
+    if @contact_form.valid?
+      redirect_to contact_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def contact_form_params
+    params.require(:contact_form).permit!
   end
 end
--- app/models/contact_form.rb
+++ app/models/contact_form.rb
@@ -1,7 +1,19 @@
 class ContactForm
   include ActiveModel::Model
 
+  TOPICS = ["General", "Print or Bulk Orders", "Sales and Purchasing", "Professional Development", "Support"]
+  SUPPORT_TYPES = ["Order Support", "Parent Support", "Content/Implementation Support", "Content Error", "Technical Support"]
+
   attr_accessor :topic, :support_type, :first_name, :last_name, :email, :phone, :role, :school_district_name, :school_district_type,
     :country, :state, :curriculum, :grade, :school_district_size, :title_1, :returning_customer, :tax_exempt, :tax_exempt_id, :desired_dates,
     :desired_training_topic, :items_purchased, :format, :description, :school_district
+
+  validates_presence_of :first_name, :last_name, :email, :phone
+  validates_presence_of :description, if: :require_description?
+  validates_presence_of :format, if: ->{ self.support_type == 'Content Error' }
+
+  def require_description?
+    ['General', 'Professional Development'].include?(self.topic) || 
+      ( self.topic == 'Support' && ['Order Support', 'Parent Support', 'Content/Implementation Support', 'Technical Support'].include?(self.support_type) )
+  end
 end
