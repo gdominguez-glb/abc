3705bea2fa354fb70666ca3b1cd69bd61496f6f3
Adam Frazin <adam@mobomo.com>
2015-10-23 17:12:04 -0400

    Add validation for 18-character SF IDs

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -73,6 +73,9 @@ Spree::Product.class_eval do
 
   validates :redirect_url, format: { with: URI.regexp }, allow_blank: true
   validates :sf_id_product, :sf_id_pricebook, presence: true, unless: :free?
+  validates :sf_id_product, :sf_id_pricebook,
+            allow_blank: true, format: { with: /\A[0-9a-zA-Z]{18}\Z/ },
+            unless: :free?
 
   belongs_to :curriculum, class_name: 'Spree::Curriculum'
   belongs_to :grade, class_name: 'Spree::Grade'
