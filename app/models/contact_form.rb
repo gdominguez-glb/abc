028fbda578e40219beccd033079a451dacc7e87f
Roc Yu <rociiu.yu@gmail.com>
2015-10-27 10:45:42 +0800

    GM-675 remove Tax Excempt fields from contact form

--- app/models/contact_form.rb
+++ app/models/contact_form.rb
@@ -98,12 +98,21 @@ class ContactForm
       'Description' => self.description,
       'Intrested_in_hosting_an_open_enrollment__c' => self.interested_in_hosting_events,
       'Request_Date__c'  => Date.today.to_s,
-      'RecordTypeId' => lead_pd_request_record_type_id
+      'RecordTypeId' => lead_pd_request_record_type_id,
+      'OwnerId' => pd_lead_queue_id
     }
   end
 
   def lead_pd_request_record_type_id
     RecordType.find_in_salesforce_by_name_and_object_type('PD Request', 'Lead').try('Id')
+  end
+
+  def pd_lead_queue_id
+    Rails.cache.fetch(:pd_lead_queue_id) do
+      GmSalesforce::Client.instance.find_all_in_salesforce('Group', 'Id', "Name='PD Request Lead'").first.Id
+    end
+  rescue
+    nil
   end
 
   def support_attributes
