5a89800784d5c2ddd8904f028013b72b405b60a1
Roc Yu <rociiu.yu@gmail.com>
2015-10-28 08:38:27 +0800

    GM-668 generate filter item with remove icon in video gallery side filters

--- app/helpers/video_gallery_helper.rb
+++ app/helpers/video_gallery_helper.rb
@@ -11,13 +11,26 @@ module VideoGalleryHelper
   end
 
   def video_gallery_taxon_item(taxon, sibling_ids, allow_multiple_taxons_selected)
-    css_class = (params[:taxon_ids].include?(taxon.id.to_s)) ? 'list-group-item active' : 'list-group-item'
-    taxon_ids = generate_taxon_ids_param(taxon, params[:taxon_ids], sibling_ids, allow_multiple_taxons_selected)
-    link_to(taxon.name, params.merge(taxon_ids: taxon_ids), class: css_class)
+    taxon_selected = params[:taxon_ids].include?(taxon.id.to_s)
+    css_class = taxon_selected ? 'list-group-item active' : 'list-group-item'
+    if taxon_selected
+      content_tag :span, class: css_class do
+        (taxon.name + "<a href='#{remove_taxon_link(taxon)}'><span class='pull-right glyphicon glyphicon-remove'></span></a>").html_safe
+      end
+    else
+      taxon_ids = generate_taxon_ids_param(taxon, params[:taxon_ids], sibling_ids, allow_multiple_taxons_selected)
+      link_to(taxon.name, params.merge(taxon_ids: taxon_ids), class: css_class)
+    end
   end
 
   def generate_taxon_ids_param(taxon, taxon_ids, sibling_ids, allow_multiple_taxons_selected)
     return taxon_ids + [taxon.id] if allow_multiple_taxons_selected
     taxon_ids.reject{|_id| sibling_ids.include?(_id.to_i)} + [taxon.id]
+  end
+
+  def remove_taxon_link(taxon)
+    taxon_ids = params[:taxon_ids].dup
+    taxon_ids.delete(taxon.id.to_s)
+    url_for(taxon_ids: taxon_ids)
   end
 end
