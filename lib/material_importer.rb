45b25ac1df53ac831b2315caea135621578d0981
Roc Yu <rociiu.yu@gmail.com>
2015-10-29 13:21:44 +0800

    GM-538 refactor:  extract MaterialZipImporter from MaterialImporter for common zip file importer

--- lib/material_importer.rb
+++ lib/material_importer.rb
@@ -1,7 +1,4 @@
-require 'fileutils'
-require 'net/http'
-require 'zip/zip'
-require 'find'
+require 'material_zip_importer'
 
 class MaterialImporter
   def import_from_material_job(material_import_job_id)
@@ -12,8 +9,12 @@ class MaterialImporter
       FileUtils.mkdir_p(tmp_directory_path(material_import_job))
 
       material_import_job.file.copy_to_local_file(:original, zip_file_path(material_import_job))
-      extract_zip_file(zip_file_path(material_import_job), raw_file_directory_path(material_import_job))
-      process_root_directory(material_import_job)
+
+      MaterialZipImporter.new(
+        product: material_import_job.product,
+        zip_file_path: zip_file_path(material_import_job),
+        dest_directory_path: raw_file_directory_path(material_import_job)
+      ).import
 
       material_import_job.update(status: 'done')
 
@@ -36,53 +37,4 @@ class MaterialImporter
     File.join(tmp_directory_path(material_import_job), 'raw')
   end
 
-  def extract_zip_file(zip_file_path, dest_directory)
-    Zip::ZipFile.open(zip_file_path) do |zip_file|
-      zip_file.each do |entry|
-        file_path= File.join(dest_directory, entry.name)
-        FileUtils.mkdir_p(File.dirname(file_path))
-        zip_file.extract(entry, file_path) unless File.exist?(file_path)
-        puts "Extracting #{entry.name}"
-      end
-    end
-  end
-
-  def process_root_directory(material_import_job)
-    product             = material_import_job.product
-    root_directory_path = File.join(raw_file_directory_path(material_import_job), product.name)
-    return unless File.exists?(root_directory_path)
-    dir = Dir.new(root_directory_path)
-    dir.entries.each do |sub_dir_name|
-      sub_dir_path = File.join(root_directory_path, sub_dir_name)
-      next if sub_dir_name.start_with?('.')
-      next if !File.directory?(sub_dir_path)
-      process_directory(product, nil, sub_dir_path)
-    end
-  end
-
-  def process_directory(product, parent, directory_path)
-    dir = Dir.new(directory_path)
-    material = Spree::Material.create(
-      name: File.basename(directory_path),
-      parent: parent,
-      product: product
-    )
-    dir.entries.each do |file_name|
-      next if file_name.start_with?('.')
-      path = File.join(directory_path, file_name)
-      if File.directory?(path)
-        process_directory(product, material, path)
-      else
-        create_material_file(material, path)
-      end
-    end
-  end
-
-  def create_material_file(material, file_path)
-    file = File.new(file_path)
-    Spree::MaterialFile.create(
-      material: material,
-      file: file
-    )
-  end
 end
