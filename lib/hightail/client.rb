5631c2e7626c9c450f8e0978e77404ded0d138e9
Roc Yu <rociiu.yu@gmail.com>
2015-10-29 08:21:54 +0800

    GM-538 update hightail sdk to include method to download file

--- lib/hightail/client.rb
+++ lib/hightail/client.rb
@@ -18,13 +18,22 @@ module Hightail
     end
 
     def folder_info(folder_id)
-      res = HTTParty.get(
-        (ENV['hightail_api_url'] + "dpi/v1/folder/#{folder_id}"),
-        {
-          headers: { 'X-Api-Key' => ENV['hightail_api_key'], 'Accept' => 'application/json', 'X-Original-User-Agent' => 'API', 'X-Auth-Token' => @auth_token }
-        }
-      )
+      url = ENV['hightail_api_url'] + "dpi/v1/folder/#{folder_id}"
+      res = HTTParty.get(url , { headers: headers })
+      res.parsed_response
     end
 
+    def download_file(file_id, dest_path)
+      url = ENV['hightail_api_url'] + "dpi/v1/folder/file/#{file_id}"
+      res = HTTParty.get(url, { headers: headers })
+      File.open(dest_path, 'wb') do |f|
+        download_url =ENV['hightail_api_url'] +  res.parsed_response['downloadUrl']
+        f.write HTTParty.get(download_url, headers: headers).parsed_response
+      end
+    end
+
+    def headers
+      { 'X-Api-Key' => ENV['hightail_api_key'], 'Accept' => 'application/json', 'X-Original-User-Agent' => 'API', 'X-Auth-Token' => @auth_token }
+    end
   end
 end
