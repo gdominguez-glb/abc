edce7791e9906bbc8d3bcc12517ce1a68feda62e
Roc Yu <rociiu.yu@gmail.com>
2015-10-29 18:32:00 +0800

    GM-537 implement VimeoImporter to import vimeo videos from csv, add task to run the importer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151028005005) do
+ActiveRecord::Schema.define(version: 20151029092959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1582,6 +1582,7 @@ ActiveRecord::Schema.define(version: 20151028005005) do
     t.string   "wistia_thumbnail_url"
     t.integer  "video_group_id"
     t.string   "preview_image_url"
+    t.string   "vimeo_id"
   end
 
   create_table "spree_zone_members", force: :cascade do |t|
---
+++ lib/vimeo_importer.rb
@@ -0,0 +1,61 @@
+require "open-uri"
+require 'gm_vimeo'
+
+class VimeoImporter
+  attr_accessor :csv_path
+
+  def initialize(csv_path)
+    @csv_path = csv_path
+  end
+
+  def import
+    puts 'start importing videos...'
+    count = 0
+    CSV.foreach(csv_path, encoding: 'ISO-8859-1', headers: true) do |row|
+      next if row['Vimeo URL'].blank?
+      import_video(row)
+      count += 1
+    end
+    puts
+    puts "imported #{count} videos."
+  end
+
+  def import_video(row)
+    print '.'
+
+    title            = row['Video Title']
+    vimeo_id         = row['Vimeo URL'].split('/').last
+    video_group_name = row['Group']
+
+    download_url = GmVimeo.new.download_url_of_video(vimeo_id)
+
+    video = Spree::Video.find_or_initialize_by(vimeo_id: vimeo_id)
+    video.update(
+      title: title,
+      video_group_name: video_group_name,
+      file: open(download_url)
+    )
+
+    assign_taxons_to_video(video, row)
+  end
+
+  def assign_taxons_to_video(video, row)
+    grades      = ['A', 'B', 'C'].map{|c| row["Grade #{c}"] }.reject(&:blank?)
+    grade_bands = ['A', 'B', 'C', 'D'].map{|c| row["Grade Band #{c}"] }.reject(&:blank?)
+    modules     = ['A', 'B', 'C'].map{|c| row["Module #{c}"] }.reject(&:blank?)
+    lessons     = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'].map{|c| row["Lesson #{c}"] }.reject(&:blank?)
+
+    assign_taxons(video, 'Grade', grades)
+    assign_taxons(video, 'Grade Band', grade_bands)
+    assign_taxons(video, 'Module', modules)
+    assign_taxons(video, 'Lesson', lessons)
+  end
+
+  def assign_taxons(video, taxonomy_name, taxon_names)
+    taxonomy = Spree::Taxonomy.find_by(name: taxonomy_name)
+    taxon_names.each do |taxon_name|
+      taxon = taxonomy.taxons.find_or_create_by(name: taxon_name)
+      video.taxons << taxon
+    end
+  end
+end
