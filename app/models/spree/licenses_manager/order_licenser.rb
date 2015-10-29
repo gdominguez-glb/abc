f4cddf7cfb08a2b567166a2bd116c93935221f4a
Adam Frazin <adam@mobomo.com>
2015-10-29 09:54:53 -0400

    Create Salesforce Assets after initial License creation

--- app/models/spree/licenses_manager/order_licenser.rb
+++ app/models/spree/licenses_manager/order_licenser.rb
@@ -1,19 +1,24 @@
 module Spree
   module LicensesManager
+    # OrderLicenser
     class OrderLicenser
-
       def initialize(order)
         @order = order
       end
 
       def execute
         @order.line_items.each do |line_item|
-          create_license(product: line_item.variant.product, quantity: line_item.quantity)
+          create_license(product: line_item.variant.product,
+                         quantity: line_item.quantity)
         end
       end
 
-      def create_license(attrs={})
-        licensed_product = Spree::LicensedProduct.create!({ order: @order, user: @order.user, skip_salesforce_create: true }.merge(attrs))
+      def create_license(attrs = {})
+        licensed_product = Spree::LicensedProduct.create!({
+          order: @order, user: @order.user, skip_salesforce_create: true
+        }.merge(attrs))
+
+        licensed_product.skip_salesforce_create = false
         if licensed_product.quantity > 1
           licensed_product.update(can_be_distributed: true)
           SingleLicenseExtractor.new(licensed_product).execute
@@ -21,7 +26,6 @@ module Spree
         licensed_product.create_in_salesforce
         licensed_product
       end
-
     end
   end
 end
