6d1bf0e5c61ea1a17bd80c4faaf68b5866eac21c
Roc Yu <rociiu.yu@gmail.com>
2015-10-30 14:21:05 +0800

    GM-678 include NewsLetter and Updates checkbox in signup form

--- app/controllers/spree/user_registrations_controller_decorator.rb
+++ app/controllers/spree/user_registrations_controller_decorator.rb
@@ -27,7 +27,7 @@ Spree::UserRegistrationsController.class_eval do
   private
 
   def spree_user_params
-    new_params = params.require(:spree_user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :title, :school_id, :district_id, school_district_attributes: [:name, :state_id, :place_type], interested_subjects: [])
+    new_params = params.require(:spree_user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :title, :school_id, :district_id, :allow_communication, school_district_attributes: [:name, :state_id, :place_type], interested_subjects: [])
     new_params[:school_district_id] = new_params[:school_id].blank? ? new_params[:district_id] : new_params[:school_id]
     if %w(Parent Homeschooler).member?(new_params[:title])
       new_params.delete(:school_district_id)
