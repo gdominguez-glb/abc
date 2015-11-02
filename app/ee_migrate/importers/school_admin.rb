fea61ea6cbe9a7535cf3489092fbfac03f392e9a
Roc Yu <rociiu.yu@gmail.com>
2015-11-02 19:33:44 +0800

    GM-806 add script to import relationship of sub-admin to legacy users

--- app/ee_migrate/importers/school_admin.rb
+++ app/ee_migrate/importers/school_admin.rb
@@ -2,15 +2,6 @@ module Importers
   class SchoolAdmin
 
     def self.import
-      # sql = <<-SQL
-      # select  members.email as Admin
-      # from exp_commoncore_credits credits
-      # join exp_channel_titles orders
-      # on entry_id = order_id
-      # join exp_members members
-      # on members.member_id = orders.author_id
-      # group by members.email
-      # SQL
       Migrate::Member.select(['exp_members.member_id, exp_members.email']).
         joins("join exp_commoncore_credits on exp_members.member_id = exp_commoncore_credits.member_id").
         joins("join exp_channel_titles on exp_channel_titles.entry_id = exp_commoncore_credits.order_id").
@@ -20,5 +11,14 @@ module Importers
         end
     end
 
+    def self.import_sub_admins
+      Migrate::Administrator.where('admin_member_id != 21775').find_each do |administrator|
+        admin = Legacy::User.find_by(ee_id: administrator.account_member_id)
+        member = Legacy::User.find_by(ee_id: administrator.admin_member_id)
+        if admin && admin.is_school_admin && member
+          member.update(is_sub_admin: true, parent_admin_id: admin.id)
+        end
+      end
+    end
   end
 end
