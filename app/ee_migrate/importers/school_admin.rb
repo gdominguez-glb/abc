f5fe908ca501186aac8e43e75e76ea4830b74e14
Roc Yu <rociiu.yu@gmail.com>
2015-11-18 12:46:59 +0800

    update query on importing school admin from web 1.0

--- app/ee_migrate/importers/school_admin.rb
+++ app/ee_migrate/importers/school_admin.rb
@@ -2,13 +2,10 @@ module Importers
   class SchoolAdmin
 
     def self.import
-      Migrate::Member.select(['exp_members.member_id, exp_members.email']).
-        joins("join exp_commoncore_credits on exp_members.member_id = exp_commoncore_credits.member_id").
-        joins("join exp_channel_titles on exp_channel_titles.entry_id = exp_commoncore_credits.order_id").
-        group("exp_members.email").find_each do |member|
-          user = Legacy::User.find_by(email: member.email)
-          user.update(is_school_admin: true) if user
-        end
+      Migrate::Member.find_by_sql("select email from (select authors.email, count(*) as licenses_count from exp_commoncore_credits credits join exp_channel_titles orders on orders.entry_id = credits.order_id join exp_members authors on authors.member_id = orders.author_id  group by authors.email) a where a.licenses_count > 1").find_each do |member|
+        user = Legacy::User.find_by(email: member.email)
+        user.update(is_school_admin: true) if user
+      end
     end
 
     def self.import_sub_admins
@@ -16,6 +13,7 @@ module Importers
         admin = Legacy::User.find_by(ee_id: administrator.account_member_id)
         member = Legacy::User.find_by(ee_id: administrator.admin_member_id)
         if admin && admin.is_school_admin && member
+          admin.update(is_school_admin: true)
           member.update(is_sub_admin: true, parent_admin_id: admin.id)
         end
       end
