8d10431ac23d22d544bdba7848588508554e51a7
Roc Yu <rociiu.yu@gmail.com>
2015-11-18 15:29:24 +0800

    use each instead of find_each for array

--- app/ee_migrate/importers/school_admin.rb
+++ app/ee_migrate/importers/school_admin.rb
@@ -2,7 +2,7 @@ module Importers
   class SchoolAdmin
 
     def self.import
-      Migrate::Member.find_by_sql("select email from (select authors.email, count(*) as licenses_count from exp_commoncore_credits credits join exp_channel_titles orders on orders.entry_id = credits.order_id join exp_members authors on authors.member_id = orders.author_id  group by authors.email) a where a.licenses_count > 1").find_each do |member|
+      Migrate::Member.find_by_sql("select email from (select authors.email, count(*) as licenses_count from exp_commoncore_credits credits join exp_channel_titles orders on orders.entry_id = credits.order_id join exp_members authors on authors.member_id = orders.author_id  group by authors.email) a where a.licenses_count > 1").each do |member|
         user = Legacy::User.find_by(email: member.email)
         user.update(is_school_admin: true) if user
       end
