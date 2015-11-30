58870511c3dfb0e6bdd2dd3282d7d011ac797aca
Roc Yu <rociiu.yu@gmail.com>
2015-11-30 16:57:40 +0800

    GM-940 update school/district dropdowns in registration to a hidden input to custom search result in select

--- app/assets/javascripts/user_form.js
+++ app/assets/javascripts/user_form.js
@@ -53,8 +53,6 @@ $(document).on("change", "#spree_user_title", function(){
   }
 
   $('#spree_user_interested_subjects').select2();
-  $('#spree_user_school_id').select2();
-  $('#spree_user_district_id').select2();
 });
 
 $(document).on('change', '#schoolDistrictSelect input', function(e) {
@@ -62,128 +60,142 @@ $(document).on('change', '#schoolDistrictSelect input', function(e) {
 
   if(schoolIsSelected) {
     $('#rowSchoolSelect').collapse('show');
+    $('#rowAddSchool input').prop('disabled', false);
+
     $('#rowDistrictSelect').collapse('hide');
-    $('#rowDistrictSelect #spree_user_district_id').val(null);
     $('#rowAddDistrict').collapse('hide');
+    $('#rowDistrictSelect #spree_user_district_id').val(null);
     $('#rowAddDistrict input').prop('disabled', true);
 
-    $('#rowAddSchool input').prop('disabled', false);
   } else {
-    $('#rowSchoolSelect').collapse('hide');
     $('#rowDistrictSelect').collapse('show');
-    $('#rowDistrictSelect #spree_user_school_id').val(null);
-    $('#rowAddSchool').collapse('hide');
-    $('#rowAddSchool input').prop('disabled', true);
-
     $('#rowAddDistrict input').prop('disabled', false);
+
+    $('#rowSchoolSelect').collapse('hide');
+    $('#rowAddSchool').collapse('hide');
+    $('#rowSchoolSelect #spree_user_school_id').val(null);
+    $('#rowAddSchool input').prop('disabled', true);
   }
 
 });
 
 $(document).on('change', '#spree_user_school_id', function() {
-  if( $(this).find('option:selected').attr('id') === '#schoolNotListed' ) {
+  if( $(this).val() === '#schoolNotListed' ) {
     $('#rowAddSchool').collapse('show');
   }
 });
 
 $(document).on('change', '#spree_user_district_id', function() {
-  if( $(this).find('option:selected').attr('id') === '#districtNotListed' ) {
+  if( $(this).val() === '#districtNotListed' ) {
     $('#rowAddDistrict').collapse('show');
   }
 });
 
 $(document).on('click', '#closeAddSchool', function() {
   $('#rowAddSchool').collapse('hide');
-  $('#rowSchoolSelect select').prop('selectedIndex',0).prev().text('Select a School');
-  $('#spree_user_school_id').select2();
 });
 
 $(document).on('click', '#closeAddDistrict', function() {
   $('#rowAddDistrict').collapse('hide');
-  $('#rowDistrictSelect select').prop('selectedIndex',0).prev().text('Select a District');
-  $('#spree_user_district_id').select2();
-});
-
-$(document).on('click', '#btnAddSchool', function() {
-  var newSchool = $('#rowAddSchool input').val();
-  var newSchoolTrim = newSchool.replace(/ /g,'');
-
-  $('#rowAddSchool').collapse('hide');
-  $('#rowSchoolSelect select').append('<option value="' + newSchoolTrim + '">' + newSchool + '<option>');
-  $('#rowSchoolSelect select').select2('val', newSchoolTrim);
-});
-
-$(document).on('click', '#btnAddDistrict', function() {
-  var newDistrict = $('#rowAddDistrict input').val();
-  var newDistrictTrim = newDistrict.replace(/ /g,'');
-
-  $('#rowAddDistrict').collapse('hide');
-  $('#rowDistrictSelect select').append('<option value="' + newDistrictTrim + '">' + newDistrict + '<option>');
-  $('#rowDistrictSelect select').select2('val', newDistrictTrim);
 });
 
 $(function(){
+  var selectedStateId = $("#spree_user_school_district_attributes_state_id").val();
+  updateSchoolDistrictSelect(parseInt(selectedStateId));
+
   $("#spree_user_title").trigger('change');
-  appendNotListed();
 
   $("#spree_user_school_district_attributes_state_id").change(function(){
     var stateId = $(this).val();
-    updateSchoolDistrictSelect(stateId);
+    updateSchoolDistrictSelect(parseInt(stateId));
   });
 
   function updateSchoolDistrictSelect(_stateId) {
     var stateSchoolData, stateDistrictData;
-    if (_stateId) {
-      var stateId = parseInt(_stateId);
-      stateSchoolData = findStateData(window.schoolData, stateId);
-      stateDistrictData = findStateData(window.districtData, stateId);
-    } else {
-      stateSchoolData = window.schoolData;
-      stateDistrictData = window.districtData;
-    }
+    var stateId = parseInt(_stateId);
+    stateSchoolData = findStateData(window.schoolData, stateId);
+    stateDistrictData = findStateData(window.districtData, stateId);
 
-    renderSchoolOptions(stateSchoolData);
-    renderDistrictOptions(stateDistrictData);
+    renderSchoolOptions(stateSchoolData, _stateId);
+    renderDistrictOptions(stateDistrictData, _stateId);
   }
 
-  function renderSchoolOptions(data) {
-    var options = ['<option>Select a School </option>', '<option id="#schoolNotListed">School Not Listed</option>'];
-    options = options.concat(generateOptions(data));
-    updateSelectWithOption($('#spree_user_school_id'), options);
+  function renderSchoolOptions(data, stateId) {
+    $('#spree_user_school_id').select2('destroy');
+    $('#spree_user_school_id').select2({
+      data: data,
+      placeholder: 'Select A School',
+      initSelection: function(element, callback){
+        var id = $(element).val();
+        var selectedData = findSelectedData(data, id);
+        callback(selectedData);
+      },
+      query: function(query) {
+        var data = searchSchool(query.term, stateId);
+        query.callback(data);
+      }
+    });
   }
 
-  function renderDistrictOptions(data) {
-    var options = ['<option>Select a District </option>', '<option id="#districtNotListed">District Not Listed</option>'];
-    options = options.concat(generateOptions(data));
-    updateSelectWithOption($('#spree_user_district_id'), options);
-  }
-
-  function updateSelectWithOption($select, options) {
-    $select.html(options.join(''));
-    $select.select2('destroy');
-    $select.select2();
-  }
-
-  function generateOptions(data) {
-    var options = [];
-    for(var i = 0; i < data.length; i ++) {
-      options.push("<option value='"+data[i].id+"'>"+data[i].name+"</option>");
-    }
-    return options;
+  function renderDistrictOptions(data, stateId) {
+    $('#spree_user_district_id').select2('destroy');
+    $('#spree_user_district_id').select2({
+      data: data,
+      placeholder: 'Select A District',
+      initSelection: function(element, callback){
+        var id = $(element).val();
+        var selectedData = findSelectedData(data, id);
+        callback(selectedData);
+      },
+      query: function(query) {
+        var data = searchDistrict(query.term, stateId);
+        query.callback(data);
+      }
+    });
   }
 
   function findStateData(data, stateId) {
+    if(!stateId) {
+      return data;
+    }
     var matches = [];
+    var notListRegx = new RegExp('Not Listed', 'i');
     for(var i = 0; i < data.length; i ++) {
-      if(data[i].state_id === stateId) {
+      if(data[i].state_id === stateId || notListRegx.test(data[i].text)) {
         matches.push(data[i]);
       }
     }
     return matches;
   }
 
-  function appendNotListed() {
-    $("#spree_user_school_id option:eq(0)").after('<option id="#schoolNotListed">School Not Listed</option>');
-    $("#spree_user_district_id option:eq(0)").after('<option id="#districtNotListed">District Not Listed</option>');
+  function searchDistrict(term, stateId) {
+    var data = { results: [] };
+    var _districtData = findStateData(window.districtData, stateId);
+    for(var i = 0; i < _districtData.length; i ++) {
+      if(!term || _districtData[i].text === 'District Not Listed' || _districtData[i].text.search(new RegExp(term, 'i')) >= 0) {
+        data.results.push(_districtData[i]);
+      }
+    }
+    return data;
+  }
+
+  function searchSchool(term, stateId) {
+    var data = { results: [] };
+    var _schoolData = findStateData(window.schoolData, stateId);
+    for(var i = 0; i < _schoolData.length; i ++) {
+      if(!term || _schoolData[i].text === 'School Not Listed' || _schoolData[i].text.search(new RegExp(term, 'i')) >= 0) {
+        data.results.push(_schoolData[i]);
+      }
+    }
+    return data;
+  }
+
+  function findSelectedData(data, id) {
+    for(var i = 0; i < data.length; i ++) {
+      if(data[i].id === parseInt(id)) {
+        return data[i];
+      }
+    }
+    return null;
   }
 });
