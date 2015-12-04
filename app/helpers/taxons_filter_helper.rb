73b05590fa0bdf319db65cee7ec4c1e9a67655be
Roc Yu <rociiu.yu@gmail.com>
2015-12-04 11:04:58 +0800

    GM-869 refactor taxons filter helper to use in store/video filters

---
+++ app/helpers/taxons_filter_helper.rb
@@ -0,0 +1,43 @@
+module TaxonsFilterHelper
+  include Spree::BaseHelper
+
+  def gm_taxons_tree(root_taxon, current_taxon, allow_multiple_taxons_selected)
+    content_tag :ul, class: 'list-group' do
+      sibling_ids = root_taxon.children.map(&:id)
+      root_taxon.children.map do |taxon|
+        gm_taxon_item(taxon, sibling_ids, allow_multiple_taxons_selected)
+      end.join("\n").html_safe
+    end
+  end
+
+  def gm_taxon_item(taxon, sibling_ids, allow_multiple_taxons_selected)
+    taxon_selected = params[:taxon_ids].map(&:to_s).include?(taxon.id.to_s)
+    if taxon_selected
+      generate_selected_taxon_item(taxon)
+    else
+      generate_normal_taxon_item(taxon, sibling_ids, allow_multiple_taxons_selected)
+    end
+  end
+
+  def generate_selected_taxon_item(taxon)
+    content_tag :a, :href => remove_taxon_link(taxon), class: 'list-group-item active' do
+      (taxon.name + "<span class='badge badge-danger'><i class='mi'>close</i></span>").html_safe
+    end
+  end
+
+  def generate_normal_taxon_item(taxon, sibling_ids, allow_multiple_taxons_selected)
+    taxon_ids = generate_taxon_ids_param(taxon, params[:taxon_ids], sibling_ids, allow_multiple_taxons_selected)
+    link_to(taxon.name, params.merge(taxon_ids: taxon_ids), class: 'list-group-item')
+  end
+
+  def generate_taxon_ids_param(taxon, taxon_ids, sibling_ids, allow_multiple_taxons_selected)
+    return taxon_ids + [taxon.id] if allow_multiple_taxons_selected
+    taxon_ids.reject{|_id| sibling_ids.include?(_id.to_i)} + [taxon.id]
+  end
+
+  def remove_taxon_link(taxon)
+    taxon_ids = params[:taxon_ids].dup.map(&:to_s)
+    taxon_ids.delete(taxon.id.to_s)
+    url_for(taxon_ids: taxon_ids)
+  end
+end
