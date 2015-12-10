fb9e1955d9aa1514be9a50e4d77564e586807180
Roc Yu <rociiu.yu@gmail.com>
2015-12-10 16:26:04 +0800

    GM-1012 assign licenses to license admin if specify in purchase

--- app/models/spree/licenses_manager/order_licenser.rb
+++ app/models/spree/licenses_manager/order_licenser.rb
@@ -7,15 +7,17 @@ module Spree
       end
 
       def execute
+        email = (@order.license_admin_email || @order.user.try(:email) || @order.email)
         @order.line_items.each do |line_item|
           create_license(product: line_item.variant.product,
+                         email: email,
                          quantity: line_item.quantity)
         end
       end
 
       def create_license(attrs = {})
         licensed_product = Spree::LicensedProduct.create!({
-          order: @order, user: @order.user, skip_salesforce_create: true
+          order: @order, skip_salesforce_create: true
         }.merge(attrs))
 
         licensed_product.skip_salesforce_create = false
