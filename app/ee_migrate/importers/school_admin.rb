51a5c80f663614f43e9dd6405885948eb2d89f6a
Roc Yu <rociiu.yu@gmail.com>
2015-12-16 14:28:44 +0800

    GM-1074 fix bug that secondary admin not assign correctly

--- app/ee_migrate/importers/school_admin.rb
+++ app/ee_migrate/importers/school_admin.rb
@@ -10,7 +10,7 @@ module Importers
       Migrate::Administrator.where('admin_member_id != 21775').find_each do |administrator|
         admin = Legacy::User.find_by(ee_id: administrator.account_member_id)
         member = Legacy::User.find_by(ee_id: administrator.admin_member_id)
-        if admin && admin.is_school_admin && member
+        if admin && member
           admin.update(is_school_admin: true)
           member.update(is_sub_admin: true, parent_admin_id: admin.id)
         end
