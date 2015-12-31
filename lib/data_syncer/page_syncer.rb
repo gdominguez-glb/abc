68d9f8372ee8c71f8fb7aae35bc2f620ef92cf39
Roc Yu <rociiu.yu@gmail.com>
2015-12-31 15:20:36 +0800

    GM-667 convert staging url to production url when sync page data

--- lib/data_syncer/page_syncer.rb
+++ lib/data_syncer/page_syncer.rb
@@ -3,8 +3,24 @@ class DataSyncer::PageSyncer < DataSyncer::Base
     Page.all.map do |page|
       attrs = page.attributes
       curriculum_id = attrs.delete('curriculum_id')
+      if page.tiles.present?
+        attrs['tiles'] = convert_urls_in_tiles(page.tiles)
+      end
       attrs['curriculum_name'] = Curriculum.find_by(id: curriculum_id).try(:name)
       attrs.to_hash
     end.to_yaml
+  end
+
+  def convert_urls_in_tiles(tiles)
+    rows = tiles[:rows]
+    rows.each do |row|
+      row.keys.select{|k| k =~ /link/ }.each do |key|
+        if row[key] && row[key].include?('staging.greatminds.org')
+          changed = true
+          row[key] = row[key].gsub('staging.greatminds.org', 'production.greatminds.org')
+        end
+      end
+    end
+    { tiles: rows }
   end
 end
