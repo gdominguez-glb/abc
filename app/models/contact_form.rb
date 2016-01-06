c66c0567565eec7cfcf88dcb58ffe148136cd787
Roc Yu <rociiu.yu@gmail.com>
2016-01-06 10:26:28 +0800

    GM-1164 fix error when submit PD in contact form leave desire date blank

--- app/models/contact_form.rb
+++ app/models/contact_form.rb
@@ -93,7 +93,7 @@ class ContactForm
       'State' => self.state,
       'What_curriculum_are_you_interested_in__c' => self.curriculum,
       'Session_Preference__c' => self.desired_training_topic,
-      'X1st_Date_Preference__c' => self.desired_dates,
+      'X1st_Date_Preference__c' => (self.desired_dates.blank? ? nil : self.desired_dates),
       'Grade_Training_Request__c' => (self.grade_bands || []).join(';'),
       'Size_of_Training_Groups__c' => self.training_groups_size,
       'Description' => self.description,
