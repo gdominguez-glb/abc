e7eb963d6e244fd2ee5a753c26d63fc3702898f3
Roc Yu <rociiu.yu@gmail.com>
2016-01-07 14:46:34 +0800

    add test for Spree::GroupItem

--- spec/models/spree/group_item_spec.rb
+++ spec/models/spree/group_item_spec.rb
@@ -1,4 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Spree::GroupItem, type: :model do
+  it { should belong_to(:group).class_name('Spree::Product') }
+  it { should belong_to(:product).class_name('Spree::Product') }
+
+  let(:group_product) { create(:product) }
+  let(:product) { create(:product) }
+
+  it "mark product not saleable in storefront when add to group" do
+    Spree::GroupItem.create(group: group_product, product: product)
+    expect(product.reload.show_in_storefront).to eq(false)
+  end
+
+  it "revert product to be saleable after remove from group item" do
+    group_item = Spree::GroupItem.create(group: group_product, product: product)
+    group_item.destroy
+
+    expect(product.reload.show_in_storefront).to eq(true)
+  end
 end
