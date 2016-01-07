254ebc7961e15ff849fbb6e3b2ff5cdb7a127b6a
Jeff Pelletier <jeff.pelletier@intridea.com>
2016-01-07 14:26:08 -0500

    Existing tour updates.

--- app/assets/javascripts/dashboard_tour.js
+++ app/assets/javascripts/dashboard_tour.js
@@ -3,19 +3,20 @@ function startDashboardTour() {
 
   tour = tour.addStep('resources', {
     title: 'Resources',
-    text: "You can find all the digital resources you have access to right here. This includes anything you've checked out with and any products distributed to you by an administrator",
+    text: "You can find all the digital resources you have access to right here. This includes your copy of products that you've purchased to distribute.",
     attachTo: 'li.my-resource-link bottom'
   });
 
   tour = tour.addStep('launch', {
     title: 'Launch',
     text: 'Use the launch button to navigate into your tools.',
+    attachTo: '.js-my-products bottom'
   });
 
   tour = tour.addStep('recommendations', {
     title: 'Recommendations',
     text: 'Recommendations will keep you in the loop about everything from new blog posts to professional development opportunities.',
-    attachTo: 'h4.recommends-title top'
+    attachTo: 'h4.recommends-title right'
   });
 
   tour = tour.addStep('video_gallery', {
@@ -33,6 +34,7 @@ function startDashboardTour() {
   tour = tour.addStep('support', {
     title: 'Support',
     text: "Need support? Check out our FAQ section. If you don't find what you're looking for, don't hesitate to contact us.",
+    attachTo: '.footer top',
     buttons: [
         {
           text: 'Exit',
