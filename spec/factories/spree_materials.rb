4c5f9f2dc3aa6daf94781765f74828aac814912e
Roc Yu <rociiu.yu@gmail.com>
2016-01-07 15:01:32 +0800

    improve test on Spree::Material

--- spec/factories/spree_materials.rb
+++ spec/factories/spree_materials.rb
@@ -1,8 +1,6 @@
 FactoryGirl.define do
   factory :spree_material, :class => 'Spree::Material' do
     name "MyString"
-    product_id 1
-    parent_id 1
   end
 
 end
--- spec/models/spree/material_spec.rb
+++ spec/models/spree/material_spec.rb
@@ -1,8 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Spree::Material, type: :model do
+  let(:material) { create(:spree_material) }
+
   it { should belong_to(:product).class_name('Spree::Product') }
   it { should have_many(:material_files) }
 
   it { should validate_presence_of(:name) }
+
+  describe "#should_index?" do
+    it "return true with product" do
+      material.product = create(:product)
+      expect(material.should_index?).to eq(true)
+    end
+
+    it "return false without product" do
+      material.product = nil
+      expect(material.should_index?).to eq(false)
+    end
+  end
+
+  describe "#search_data" do
+    let!(:product) { create(:product) }
+    let!(:material) { create(:spree_material, name: "Test", product: product) }
+    let!(:user) { create(:gm_user) }
+    let!(:order) { create(:order, user: user) }
+    let!(:line_item) { create(:line_item, order: order, variant: product.master) }
+
+    it "index name and user ids associate with material" do
+      expect(material.search_data).to eq({
+        name: material.name,
+        user_ids: [user.id]
+      })
+    end
+  end
 end
