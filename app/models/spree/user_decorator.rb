6b0e2c0595a25bde29026cb8da8a15d60fe4c708
Roc Yu <rociiu.yu@gmail.com>
2016-01-11 15:38:00 +0800

    GM-1122 fix issue product name not show up for auto assigned license in licenses management

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -141,7 +141,7 @@ Spree::User.class_eval do
   end
 
   def licensed_products_from(school_district_admin)
-    licensed_products.joins(:product).joins(:product_distribution).where({spree_product_distributions: { from_user_id: school_district_admin.id }}).uniq
+    Spree::LicensedProduct.where(user_id: self.id).joins(:product).joins(:product_distribution).where({spree_product_distributions: { from_user_id: school_district_admin.id }}).uniq
   end
 
   def assign_user_role
