5698a30802272f6aa3e81c9f198489ed9cdeb4a3
Roc Yu <rociiu.yu@gmail.com>
2016-01-15 13:19:17 +0800

    GM-1194 only display success notice to access on checkout when user purchase to himself

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -2,7 +2,7 @@ Spree::CheckoutController.class_eval do
 
   # override to custom page after complete order
   def completion_route(custom_params = nil)
-    if @order.all_digitals?
+    if @order.all_digitals? && @order.license_admin_email.blank?
       flash.notice = "You now have access to these products!"
     end
     if @order.free?
