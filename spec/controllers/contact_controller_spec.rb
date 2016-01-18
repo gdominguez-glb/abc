c2304e0cde773be95e9163a2e30e6cbdf8d510be
Roc Yu <rociiu.yu@gmail.com>
2016-01-18 14:38:00 +0800

    improve test coverage on contacts controller

--- spec/controllers/contact_controller_spec.rb
+++ spec/controllers/contact_controller_spec.rb
@@ -7,4 +7,22 @@ RSpec.describe ContactController, type: :controller do
       expect(response).to be_success
     end
   end
+
+  describe "POST create" do
+    it "redirect with notice when submit successfully" do
+      allow_any_instance_of(ContactForm).to receive(:valid?).and_return(true)
+      allow_any_instance_of(ContactForm).to receive(:perform)
+
+      post :create, contact_form: { first_name: 'John' }
+      expect(response).to redirect_to(contact_path)
+      expect(flash[:notice]).to eq("Thanks for reaching out. We will be in touch shortly.")
+    end
+
+    it "render index when fail to submit" do
+      allow_any_instance_of(ContactForm).to receive(:valid?).and_return(false)
+
+      post :create, contact_form: { first_name: 'John' }
+      expect(response).to render_template(:index)
+    end
+  end
 end
