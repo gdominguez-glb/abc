fcfb4680adcceec4acdb9fa23bd48d1277477cc1
Roc Yu <rociiu.yu@gmail.com>
2016-03-10 16:04:35 +0800

    GM-1290 redirect user to resources page when checkout only with 1 product

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -6,7 +6,7 @@ Spree::CheckoutController.class_eval do
       flash.notice = "You now have access to these products!"
     end
     if @order.free?
-      main_app.account_root_path
+      @order.single_purchase? ? main_app.account_products_path : main_app.account_root_path
     else
       spree.completed_order_path(@order, custom_params)
     end
