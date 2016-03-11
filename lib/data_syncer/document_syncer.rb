78233cae0fa3268eed23fe770526d65e63ddbcc6
Roc Yu <rociiu.yu@gmail.com>
2016-03-11 19:06:43 +0800

    fix issue document not sync correctly due to yaml keys

--- lib/data_syncer/document_syncer.rb
+++ lib/data_syncer/document_syncer.rb
@@ -2,10 +2,10 @@ class DataSyncer::DocumentSyncer < DataSyncer::Base
   def generate_yaml_content(klass)
     Document.all.map do |document|
       {
-        id: document.id,
-        name: document.name,
-        category: document.category,
-        remote_url: document.attachment.url(:original)
+        'id' => document.id,
+        'name' => document.name,
+        'category' => document.category,
+        'remote_url' => document.attachment.url(:original)
       }
     end.to_yaml
   end
