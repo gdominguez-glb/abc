9fde5d0c489e948d4836e9ccf839c5561120cd04
Roc Yu <rociiu.yu@gmail.com>
2016-03-15 18:40:05 +0800

    GM-1348 add search index on blog post, add result partial for post in searches page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,18 @@
 class Post < ActiveRecord::Base
   belongs_to :medium_publication
+
+  searchkick callbacks: :async
+
+  def should_index?
+    medium_publication.present?
+  end
+
+  def search_data
+    {
+      title: title,
+      subtitle: subtitle,
+      body: body,
+      user_ids: [-1]
+    }
+  end
 end
