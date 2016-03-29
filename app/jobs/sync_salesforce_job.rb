b4faa268956e2345a4609a9c95eeafac0b612b82
Roc Yu <rociiu.yu@gmail.com>
2016-03-29 15:45:35 +0800

    pass klass instead of string into importer

--- app/jobs/sync_salesforce_job.rb
+++ app/jobs/sync_salesforce_job.rb
@@ -7,7 +7,7 @@ class SyncSalesforceJob < ActiveJob::Base
                   'Contact' => 'Spree::User',
                   'PricebookEntry' => 'Spree::Product' }
     classes.each do |name, clazz|
-      processed = SalesforceObjectImporter.new(name, clazz).import
+      processed = SalesforceObjectImporter.new(name, clazz.constantize)..import
       logger.info "#{processed} Salesforce #{name} records processed"
     end
   end
