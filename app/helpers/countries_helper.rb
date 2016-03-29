ed5e1ff1f379654790b0db80962bdb67f4863bc2
Roc Yu <rociiu.yu@gmail.com>
2016-03-29 17:24:11 +0800

    GM-1423 improve performance in signup by reducing sqls and cache countries, school districts data

--- app/helpers/countries_helper.rb
+++ app/helpers/countries_helper.rb
@@ -1,8 +1,10 @@
 module CountriesHelper
   # Returns a list of countries with US first, for country dropdowns
   def country_list
-    us_index = Spree::Country.all.index {|country| country[:iso] == "US"}
-    countries = Spree::Country.all.to_a
-    countries.insert(0, countries.delete_at(us_index))
+    Rails.cache.fetch(:country_list, expires_in: 1.hour) do
+      us_index = Spree::Country.all.index {|country| country[:iso] == "US"}
+      countries = Spree::Country.all.to_a
+      countries.insert(0, countries.delete_at(us_index))
+    end
   end
 end
