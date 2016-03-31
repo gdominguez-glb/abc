647a0a481b636d7bb70d165f63f806e054796c41
Roc Yu <rociiu.yu@gmail.com>
2016-03-31 20:39:19 +0800

    add test to Spree::FrontendHelper

---
+++ spec/helpers/spree/frontend_helper_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe Spree::FrontendHelper, type: :helper do
+
+  let(:product) { create(:product) }
+  let(:curriculum) { create(:curriculum, name: 'Math') }
+
+  describe "#product_type_class" do
+    it "return class with curriculum name if product belong to curriculum" do
+      product.curriculum = curriculum
+      expect(helper.product_type_class(product)).to eq('product-type-math')
+    end
+
+    it "return nil if product dont belong to curriculum" do
+      product.curriculum = nil
+      expect(helper.product_type_class(product)).to eq(nil)
+    end
+  end
+end
