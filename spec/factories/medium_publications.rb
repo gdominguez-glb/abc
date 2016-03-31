723f138f69640c261e16ff141f4fa81f131c1bba
Roc Yu <rociiu.yu@gmail.com>
2016-03-31 20:02:06 +0800

    improve test coverage on Post model

--- spec/factories/medium_publications.rb
+++ spec/factories/medium_publications.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :medium_publication do
     title "MyString"
     url "MyString"
+    slug "my-string"
   end
 
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,4 +2,26 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   it { should belong_to(:medium_publication) }
+
+  let(:post) { create(:post, medium_publication: create(:medium_publication), title: 'A', subtitle: 'a', body: 'post body') }
+
+  describe "#should_index?" do
+    it "index when medium publication is present" do
+      expect(post.should_index?).to eq(true)
+    end
+
+    it "not index when medium publication is empty" do
+      post.medium_publication = nil
+      expect(post.should_index?).to eq(false)
+    end
+  end
+
+  it "generate search data" do
+    expect(post.search_data).to eq({
+      title: 'A',
+      subtitle: 'a',
+      body: 'post body',
+      user_ids: [-1]
+    })
+  end
 end
