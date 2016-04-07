ef671c3b2e42d6877d4412f0af53109d9e60df52
Jeff Pelletier <jeff.pelletier@intridea.com>
2016-04-07 09:30:31 -0400

    Include site header in Spree admin & override Bootstrap gric classes coming from Spree

--- app/assets/javascripts/spree_admin.js
+++ app/assets/javascripts/spree_admin.js
@@ -5,4 +5,26 @@ $(document).ready(function(){
 
     wrapper.addClass("sidebar-minimized");
   }
+
+  var sidebar_toggle = $('#sidebar-toggle');
+
+  sidebar_toggle.on('click', function(){
+    var wrapper = $('#wrapper');
+    var main    = $('#main-part');
+
+    if(wrapper.hasClass('sidebar-minimized')){
+      wrapper.removeClass('sidebar-minimized');
+      main
+        .removeClass('col-sm-12 col-md-12 sidebar-collapsed')
+        .addClass('col-sm-9 col-md-10');
+      $.cookie('sidebar-minimized', 'false', { path: '/admin' });
+    }
+    else {
+      wrapper.addClass('sidebar-minimized');
+      main
+        .removeClass('col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2')
+        .addClass('col-sm-12 col-md-12 sidebar-collapsed');
+      $.cookie('sidebar-minimized', 'true', { path: '/admin' });
+    }
+  });
 });
