d255d93e26d8999308b51c784ec80e53040196ab
Jeff Pelletier <jeff.pelletier@intridea.com>
2016-04-07 13:39:03 -0400

    Remove sidebar toggle override JS

--- app/assets/javascripts/spree_admin.js
+++ app/assets/javascripts/spree_admin.js
@@ -1,24 +1,3 @@
 $(document).ready(function(){
-
-  var sidebar_toggle = $('#sidebar-toggle');
-
-  sidebar_toggle.on('click', function(){
-    var wrapper = $('#wrapper');
-    var main    = $('#main-part');
-
-    if(wrapper.hasClass('sidebar-minimized')){
-      wrapper.removeClass('sidebar-minimized');
-      main
-        .removeClass('col-sm-12 col-md-12')
-        .addClass('col-sm-9 col-md-10');
-      $.cookie('sidebar-minimized', 'false', { path: '/admin' });
-    }
-    else {
-      wrapper.addClass('sidebar-minimized');
-      main
-        .removeClass('col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2')
-        .addClass('col-sm-12 col-md-12');
-      $.cookie('sidebar-minimized', 'true', { path: '/admin' });
-    }
-  });
+  //in case we need to add Spree admin stuff in the future
 });
