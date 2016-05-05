a728f62122ae74ef2becd2b0c8854431735204c7
Jeff Pelletier <jeff.pelletier@intridea.com>
2016-05-05 09:20:29 -0400

    Team modals don't contain images

--- app/assets/javascripts/team_modals.js
+++ app/assets/javascripts/team_modals.js
@@ -6,14 +6,10 @@ function teamModals(modalId) {
     var name = button.data('name');
     var title = button.data('title');
     var bio = button.data('bio');
-    var img = button.data('image');
 
     var modal = $(this);
     modal.find('.modal-name').text(name);
     modal.find('.modal-title strong').text(title);
     modal.find('.modal-body').html(bio);
-    if (img !== '/pictures/original/missing.png') {
-      modal.find('.modal-body').prepend('<img src="' + img + '" class="pull-right" />');
-    }
   });
 }
