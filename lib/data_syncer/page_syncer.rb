af9739f8229923598c766339092f17fdcc547ae6
Roc Yu <rociiu.yu@gmail.com>
2016-05-05 09:31:06 +0800

    GM-1550 update Page Syncer refer to greatminds.org domain change and fix the s3 file link when sync from staging

--- lib/data_syncer/page_syncer.rb
+++ lib/data_syncer/page_syncer.rb
@@ -15,9 +15,12 @@ class DataSyncer::PageSyncer < DataSyncer::Base
     rows = tiles[:rows]
     rows.each do |row|
       row.keys.select{|k| k =~ /link/ }.each do |key|
-        if row[key] && row[key].include?('staging.greatminds.org') && row[key] !~ /s3-staging\.greatminds\.org/
-          changed = true
-          row[key] = row[key].gsub('staging.greatminds.org', 'beta.greatminds.org')
+        next if row[key].blank?
+        if row[key].include?('staging.greatminds.org') && row[key] !~ /s3-staging\.greatminds\.org/
+          row[key] = row[key].gsub('staging.greatminds.org', 'greatminds.org')
+        end
+        if row[key].include?('s3-staging.greatminds.org')
+          row[key] = row[key].gsub('s3-staging.greatminds.org', 's3.greatminds.org')
         end
       end
     end
