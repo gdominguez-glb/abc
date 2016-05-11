6e77c5994d701900f481f580094ff6f69e726280
Roc Yu <rociiu.yu@gmail.com>
2016-05-11 11:01:49 +0800

    GM-1294 update reminders options and email content in reminder of licenses management

--- app/models/reminder_form.rb
+++ app/models/reminder_form.rb
@@ -2,31 +2,34 @@ class ReminderForm
   include ActiveModel::Model
 
   RECIPIENT_OPTIONS = [
-    'All Users',
     "Those users who haven't accepted their invitation yet",
     "Those users who haven't logged in in the last 30 days"
   ]
 
-  attr_accessor :from_user, :recipient, :content
-
-  validates_presence_of :content
+  attr_accessor :from_user, :recipient
 
   def perform
-    recipient_emails.each do |email|
-      ReminderMailer.remind(@from_user, email, @content).deliver
+    case @recipient
+    when RECIPIENT_OPTIONS[0]
+      send_invitation_remind
+    when RECIPIENT_OPTIONS[1]
+      send_login_remind
     end
   end
 
-  def recipient_emails
-    @emails ||= begin
-      case @recipient
-      when RECIPIENT_OPTIONS[0]
-        @from_user.to_users.pluck(:email)
-      when RECIPIENT_OPTIONS[1]
-        @from_user.product_distributions.where(to_user_id: nil).pluck(:email)
-      when RECIPIENT_OPTIONS[2]
-        @from_user.to_users.where("last_sign_in_at < ?", 30.days.ago).pluck(:email)
-      end
+  def send_invitation_remind
+    @from_user.product_distributions.where(to_user_id: nil).group_by(&:email).each do |email, distributions|
+      ReminderMailer.invitation_remind(
+        @from_user,
+        email,
+        distributions.map(&:product).map(&:name).uniq
+      ).deliver_now
+    end
+  end
+
+  def send_login_remind
+    @from_user.to_users.where("last_sign_in_at < ?", 30.days.ago).pluck(:email).each do |email|
+      ReminderMailer.login_remind(@from_user, email).deliver_now
     end
   end
 end
