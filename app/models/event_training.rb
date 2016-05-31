d859f9429a1cc9eca77d3ad5fec8622d6b332e9c
Roc Yu <rociiu.yu@gmail.com>
2016-05-31 14:48:37 +0800

    improve test coverage on EventTraining

--- app/models/event_training.rb
+++ app/models/event_training.rb
@@ -2,7 +2,7 @@ class EventTraining < ActiveRecord::Base
   validates_presence_of :title, :content
 
   def title_to_slug
-    title.gsub(/[^0-9A-Za-z]/, '')
+    title.parameterize
   end
 
   searchkick callbacks: :async
--- spec/models/event_training_spec.rb
+++ spec/models/event_training_spec.rb
@@ -3,4 +3,21 @@ require 'rails_helper'
 RSpec.describe EventTraining, type: :model do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:content) }
+
+  let(:event_training) { create(:event_training, title: 'This is a event training', content: 'test') }
+
+  describe "#title_to_slug" do
+    it "generate slug from title" do
+      expect(event_training.title_to_slug).to eq('this-is-a-event-training')
+    end
+  end
+
+  describe "#search_data" do
+    it "index title and content" do
+      expect(event_training.search_data).to eq({
+        title: 'This is a event training',
+        content: 'test'
+      })
+    end
+  end
 end
