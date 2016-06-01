5b3f622394ccc8216074fff9c025f08b04333076
Jeff Pelletier <jeff.pelletier@intridea.com>
2016-06-01 11:12:04 -0400

    Refactor tour JS to fix CodeClimate issues

--- app/assets/javascripts/dashboard_tour.js
+++ app/assets/javascripts/dashboard_tour.js
@@ -1,44 +1,46 @@
 function startDashboardTour() {
   var tour = new Shepherd.Tour({ defaults: { classes: 'shepherd-theme-arrows', scrollTo: true } });
 
-  tour = tour.addStep('resources', {
-    title: 'My Dashboard',
-    text: "You can find all the digital resources you have access to right here.",
-    attachTo: 'li.my-resource-link bottom'
-  });
+  //title, text, attachTo, showCancelLink, buttons
+
+  tour = tour.addStep('resources', window.shepherdHelper.stepFactory(
+    tour,
+    'My Dashboard',
+    "You can find all the digital resources you have access to right here.",
+    'li.my-resource-link bottom')
+  );
 
   if ($('.my-products .slick-slide').length) {
-    tour = tour.addStep('launch', {
-      title: 'Launch',
-      text: 'Use the launch button to navigate into your tools.',
-      attachTo: '.js-my-products bottom'
-    });
+    tour = tour.addStep('launch', window.shepherdHelper.stepFactory(
+      tour,
+      'Launch',
+      'Use the launch button to navigate into your tools.',
+      '.js-my-products bottom')
+    );
   }
 
-  tour = tour.addStep('recommendations', {
-    title: 'Recommendations',
-    text: 'These will keep you posted about blog posts, professional development, etc.',
-    attachTo: '.js-recommends top'
-  });
+  tour = tour.addStep('recommendations', window.shepherdHelper.stepFactory(
+    tour,
+    'Recommendations',
+    'These will keep you posted about blog posts, professional development, etc.',
+    '.js-recommends top')
+  );
 
-  tour = tour.addStep('video_gallery', {
-    title: 'Video Gallery',
-    text: 'You can visit the video gallery by clicking here or by launching a video-based product.',
-    attachTo: 'li.video-gallery-link bottom'
-  });
+  tour = tour.addStep('video_gallery', window.shepherdHelper.stepFactory(
+    tour,
+    'Video Gallery',
+    'You can visit the video gallery by clicking here or by launching a video-based product.',
+    'li.video-gallery-link bottom')
+  );
 
-  tour = tour.addStep('store', {
-    title: 'Shop',
-    text: 'Looking for more great resources? You can find them here.',
-    attachTo: 'li.store-link bottom',
-    buttons: [
-        {
-          text: 'Exit',
-          classes: 'shepherd-button-secondary',
-          action: 'shepherd.cancel'
-        }
-      ]
-  });
+  tour = tour.addStep('store', window.shepherdHelper.stepFactory(
+    tour,
+    'Shop',
+    'Looking for more great resources? You can find them here.',
+    'li.store-link bottom',
+    false,
+    true)
+  );
 
   tour.start();
 }
--- app/assets/javascripts/licenses_tour.js
+++ app/assets/javascripts/licenses_tour.js
@@ -1,33 +1,32 @@
 function startLicensesTour() {
   var tour = new Shepherd.Tour({ defaults: { classes: 'shepherd-theme-arrows', scrollTo: true } });
 
-  tour = tour.addStep('step_1', {
-    title: 'Step 1',
-    text: "You can change the selected product by using the dropdown in Step 1.",
-    attachTo: '.js-step-1 top',
-    showCancelLink: true
-  });
+  //tour, title, text, attachTo, showCancelLink, buttons
 
-  tour = tour.addStep('step_2', {
-    title: 'Step 2',
-    text: "Step 2 allows you to add users for license distribution. You can distribute licenses by entering email addresses, selecting existing users, or uploading a spreadsheet. We recommend using the spreadsheet template for bulk uploads.",
-    attachTo: '.js-step-2 top',
-    showCancelLink: true
-  });
+  tour = tour.addStep('step_1', window.shepherdHelper.stepFactory(
+    tour,
+    'Step 1',
+    'You can change the selected product by using the dropdown in Step 1.',
+    '.js-step-1 top',
+    true)
+  );
 
-  tour = tour.addStep('step_3', {
-    title: 'Step 3',
-    text: "Once you've added users, use Step 3 to determine the number of licenses to distribute to each user. Generally, you will only want to assign one license per user. Assigning more than one license to a user will allow them to manage and distribute those licenses. The number of users will display, and multiply by the number of licenses to indicate the total number of licenses that will be distributed. Click to 'Assign Licenses, and your licenses will be distributed!'",
-    attachTo: '.js-step-3 top',
-    showCancelLink: true,
-    buttons: [
-      {
-        text: 'Exit',
-        classes: 'shepherd-button-secondary',
-        action: 'shepherd.cancel'
-      }
-    ]
-  });
+  tour = tour.addStep('step_2', window.shepherdHelper.stepFactory(
+    tour,
+    'Step 2',
+    'Step 2 allows you to add users for license distribution. You can distribute licenses by entering email addresses, selecting existing users, or uploading a spreadsheet. We recommend using the spreadsheet template for bulk uploads.',
+    '.js-step-2 top',
+    true)
+  );
+
+  tour = tour.addStep('step_3', window.shepherdHelper.stepFactory(
+    tour,
+    'Step 3',
+    "Once you've added users, use Step 3 to determine the number of licenses to distribute to each user. Generally, you will only want to assign one license per user. Assigning more than one license to a user will allow them to manage and distribute those licenses. The number of users will display, and multiply by the number of licenses to indicate the total number of licenses that will be distributed. Click to 'Assign Licenses, and your licenses will be distributed!'",
+    '.js-step-3 top',
+    true,
+    true)
+  );
 
   tour.start();
 }
