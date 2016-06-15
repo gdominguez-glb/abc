04aa7c8e957c05f201bcf6fdd0f0c54349d567e0
Roc Yu <rociiu.yu@gmail.com>
2016-06-15 16:31:52 +0800

    fix legacy users migration script

--- app/ee_migrate/importers/user.rb
+++ app/ee_migrate/importers/user.rb
@@ -4,7 +4,17 @@ module Importers
       product_ids = Importers::Licenses::PRODUCTS_MAPPINGS.map {|h| h[:legacy_id]}
 
       member_ids = Migrate::Credit.where("expiration_date > ? or expiration_date is null", Date.today).where(product_id: product_ids).pluck(:member_id).compact.uniq
-      members = Migrate::Member.where(member_id: member_ids)
+      members = Migrate::Member.select([
+        'exp_members.member_id',
+        'exp_members.group_id',
+        'exp_members.email',
+        'exp_channel_data.field_id_158 as first_name',
+        'exp_channel_data.field_id_158 as last_name'
+      ]).joins("left join exp_member_data ON exp_members.member_id = exp_member_data.member_id")
+      .joins("left join exp_channel_titles ON exp_channel_titles.author_id = exp_members.member_id")
+      .joins("left join exp_channel_data ON exp_channel_titles.entry_id = exp_channel_data.entry_id")
+      .where("exp_members.id in (?)", member_ids)
+      .group("exp_members.member_id")
 
       if emails.present?
         members = members.where('exp_members.email in (?)', emails)
