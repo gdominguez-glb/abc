da81cc65dd50a9de800d1873864e3f9b24bb872f
Roc Yu <rociiu.yu@gmail.com>
2016-06-20 20:01:17 +0800

    improve test coverage on EventPage

--- spec/models/event_page_spec.rb
+++ spec/models/event_page_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe EventPage, type: :model do
   it { should belong_to(:page) }
 
   it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:slug) }
 
   describe ".displayable" do
     let!(:event_page) { create(:event_page, display: true, page: create(:page) ) }
@@ -35,6 +36,28 @@ RSpec.describe EventPage, type: :model do
     it "return regonline events with match filter" do
       regonline_event = create(:regonline_event, client_event_id: 'Math', location_name: 'A', city: 'B', state: 'C', country: 'D')
       expect(event_page.events).to eq([regonline_event])
+    end
+  end
+
+  describe "#should_index?" do
+    let(:event_page) { build(:event_page) }
+
+    it 'return true for displayable event page' do
+      event_page.display = true
+      expect(event_page.should_index?).to eq(true)
+    end
+
+    it "return false for non-displayable event page" do
+      event_page.display = false
+      expect(event_page.should_index?).to eq(false)
+    end
+  end
+
+  describe "#search_data" do
+    let(:event_page) { create(:event_page, title: 'E', description: 'D', display: true ) }
+
+    it "return data to index" do
+      expect(event_page.search_data).to eq({ title: 'E', description: 'D', user_ids: [-1] })
     end
   end
 end
