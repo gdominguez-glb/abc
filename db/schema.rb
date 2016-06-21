837a091392717240b912ab96954cdbb213da79c3
Nicolas Rodas <nicolas.rodas@mobomo.com>
2016-06-21 12:26:47 -0300

    GM-1633 Recommendations now accept a position value

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160614081141) do
+ActiveRecord::Schema.define(version: 20160621134757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -389,6 +389,7 @@ ActiveRecord::Schema.define(version: 20160614081141) do
     t.string   "icon"
     t.boolean  "display",                    default: false
     t.string   "user_title"
+    t.integer  "position",                   default: 0
   end
 
   create_table "regonline_events", force: :cascade do |t|
