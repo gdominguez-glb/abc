7005357d072e79b54fe7eb3c0ce7aaac05b2453b
Roc Yu <rociiu.yu@gmail.com>
2016-06-24 10:48:41 +0800

    GM-1764 implement domain handling for witeng.link to link uploads

--- app/models/link_file.rb
+++ app/models/link_file.rb
@@ -6,4 +6,12 @@ class LinkFile < ActiveRecord::Base
     s3_host_alias:  ENV['s3_bucket_name']
   }
   validates_attachment :file, presence: true, content_type: { content_type: /\A*\Z/ }
+
+  before_save :set_slug
+
+  private
+
+    def set_slug
+      self.slug = File.basename(self.file_file_name, File.extname(self.file_file_name))
+    end
 end
--- db/migrate/20160624014938_create_link_files.rb
+++ db/migrate/20160624014938_create_link_files.rb
@@ -1,6 +1,7 @@
 class CreateLinkFiles < ActiveRecord::Migration
   def change
     create_table :link_files do |t|
+      t.string :slug
 
       t.timestamps null: false
     end
