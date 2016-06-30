76069c75e6ee7f5777a521f1a175098adc79fe7c
Roc Yu <rociiu.yu@gmail.com>
2016-06-30 18:51:29 +0800

    GM-1781 add missing url parameter on MaterialFile

--- app/models/spree/material_file.rb
+++ app/models/spree/material_file.rb
@@ -3,6 +3,7 @@ class Spree::MaterialFile < ActiveRecord::Base
 
   has_attached_file :file,
     s3_permissions: :private,
+    url:            ":s3_alias_url",
     s3_protocol:    "https",
     s3_host_alias:  ENV['s3_bucket_name'],
     s3_headers: { "Content-Disposition" => "attachment" }
