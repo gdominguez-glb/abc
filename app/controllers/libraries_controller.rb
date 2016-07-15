a3002231bfa798d08310195b427eed8e0d7964d2
Roc Yu <rociiu.yu@gmail.com>
2016-07-15 11:00:06 +0800

    GM-1795 implement library product in account dashboard and page to list all library items

---
+++ app/controllers/libraries_controller.rb
@@ -0,0 +1,24 @@
+class LibrariesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_product, only: [:show, :launch, :download]
+
+  def show
+    @library_leafs = @product.library_leafs.includes(:library_items)
+  end
+
+  def launch
+    @library_item = @product.library_items.find(params[:item_id])
+  end
+
+  def download
+    @library_item = @product.library_items.find(params[:item_id])
+    redirect_to @library_item.attachment.expiring_url(60*60)
+  end
+
+  private
+
+  def find_product
+    @product = Spree::Product.find_by(slug: params[:id])
+    redirect_to root_path if @product.blank?
+  end
+end
