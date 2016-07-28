007ab70c9bfd201b10f37a551eef7e8ba05462c6
Roc Yu <rociiu.yu@gmail.com>
2016-07-28 12:01:07 +0800

    GM-1832 update licenses creation logic to assign school admin role when order enable single distribution or user purchase multiple licenses, update logic on assign school admin role when user register the site

--- spec/models/spree/licenses_manager/order_licenser_spec.rb
+++ spec/models/spree/licenses_manager/order_licenser_spec.rb
@@ -5,12 +5,50 @@ RSpec.describe Spree::LicensesManager::OrderLicenser do
   let!(:product) { create(:product) }
   let!(:order) { create(:order, user: user) }
 
-  before(:each) do
-    order.contents.add(product.master, 10)
-    Spree::LicensesManager::OrderLicenser.new(order).execute
+  context 'multiple licenses' do
+    before(:each) do
+      order.contents.add(product.master, 10)
+      Spree::LicensesManager::OrderLicenser.new(order).execute
+    end
+
+    it "create license from order" do
+      expect(user.licensed_products.count).to eq(2)
+    end
+
+    it "assign school admin role to user" do
+      expect(user.reload.has_school_admin_role?).to eq(true)
+    end
   end
 
-  it "create license from order" do
-    expect(user.licensed_products.count).to eq(2)
+  context "single license" do
+    before(:each) do
+      order.contents.add(product.master, 1)
+      Spree::LicensesManager::OrderLicenser.new(order).execute
+    end
+
+    it "create license from order" do
+      expect(user.licensed_products.count).to eq(1)
+    end
+
+    it "assign school admin role to user" do
+      expect(user.reload.has_school_admin_role?).to eq(false)
+    end
+  end
+
+  context "single license with distribution enable" do
+    let!(:order) { create(:order, user: user, enable_single_distribution: true) }
+
+    before(:each) do
+      order.contents.add(product.master, 1)
+      Spree::LicensesManager::OrderLicenser.new(order).execute
+    end
+
+    it "create license from order" do
+      expect(user.licensed_products.count).to eq(1)
+    end
+
+    it "assign school admin role to user" do
+      expect(user.reload.has_school_admin_role?).to eq(true)
+    end
   end
 end
