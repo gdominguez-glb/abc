974777a3c2be0e86550965c087aa5bda99452d00
Roc Yu <rociiu.yu@gmail.com>
2016-08-01 15:35:32 +0800

    GM-1834 implement method to generate unique random coupon code when create

--- app/models/spree/coupon_code.rb
+++ app/models/spree/coupon_code.rb
@@ -3,4 +3,18 @@ class Spree::CouponCode < ActiveRecord::Base
 
   has_many :coupon_code_products, class_name: 'Spree::CouponCodeProduct'
   has_many :products, class_name: 'Spree::Product', through: :coupon_code_products
+
+  before_validation :generate_code, on: :create
+
+  def generate_code
+    max_length = 8
+    self.code ||= loop do
+      random = ([*('A'..'Z'),*('0'..'9')]-%w(0 1 I O)).sample(max_length).join
+      if Spree::CouponCode.where(code: random).exists?
+        max_length += 1 if self.class.count > (10 ** max_length / 2)
+      else
+        break random
+      end
+    end
+  end
 end
