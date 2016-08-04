d179e1ee6b56795c03d808a5e6e7e4ec119e6e4b
Jeff Pelletier <jeff.pelletier@intridea.com>
2016-08-04 10:56:38 -0400

    GM-1854 feature flip ON coupon code for QA; OFF for staging & prod

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -5,3 +5,19 @@ client = Redis.new(url: ENV['REDIS_URL'])
 adapter = Flipper::Adapters::Redis.new(client)
 
 $flipper = Flipper.new(adapter)
+
+if Rails.env.development?
+  $flipper[:coupon_code].enable
+end
+
+if Rails.env.qa?
+  $flipper[:coupon_code].enable
+end
+
+if Rails.env.staging?
+  $flipper[:coupon_code].disable
+end
+
+if Rails.env.production?
+  $flipper[:coupon_code].disable
+end
