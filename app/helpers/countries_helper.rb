76b57212c2d5456f4c58b5c096cfeb38be1b9c03
Roc Yu <rociiu.yu@gmail.com>
2016-08-08 14:47:27 +0800

    GM-1858 sort country name in alphabetical order and US in first one in sign up form

--- app/helpers/countries_helper.rb
+++ app/helpers/countries_helper.rb
@@ -2,8 +2,8 @@ module CountriesHelper
   # Returns a list of countries with US first, for country dropdowns
   def country_list
     Rails.cache.fetch(:country_list, expires_in: 1.hour) do
-      us_index = Spree::Country.all.index {|country| country[:iso] == "US"}
-      countries = Spree::Country.all.to_a
+      countries = Spree::Country.order(:name).to_a
+      us_index = countries.index {|country| country[:iso] == "US"}
       countries.insert(0, countries.delete_at(us_index))
     end
   end
