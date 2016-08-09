2f3d87ce9e85f81d9819603f8766bca8f0cec506
Roc Yu <rociiu.yu@gmail.com>
2016-08-09 15:44:04 +0800

    GM-1693 fix typo on answer syncer

--- lib/data_syncer/answer_syncer.rb
+++ lib/data_syncer/answer_syncer.rb
@@ -2,7 +2,7 @@ class DataSyncer::AnswerSyncer < DataSyncer::Base
   def generate_yaml_content(klass)
     Answer.all.map do |answer|
       attrs = answer.attributes
-      attrs['content'] = convert_urls_in_content(answers['content']) if answers['content'].present?
+      attrs['content'] = convert_urls_in_content(answer['content']) if answer['content'].present?
       attrs
     end.to_yaml
   end
