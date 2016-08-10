7b3954ceb80d6bfb9b5de5f6a7e9bdafc15e6007
Roc Yu <rociiu.yu@gmail.com>
2016-08-10 11:27:46 +0800

    Add caches for places that dont change often to reduce queries to db server

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
-    @page = Page.find_by(slug: '/')
+    @page = Rails.cache.fetch(:home_page, expires_in: 2.hours) do
+      Page.find_by(slug: '/')
+    end
   end
 
   def turn_off_browser_warning
