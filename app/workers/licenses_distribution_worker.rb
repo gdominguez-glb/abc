d68f0526ab034fe9b27aa4cb5faf443b34f86d04
Roc Yu <rociiu.yu@gmail.com>
2016-08-19 10:52:08 +0800

    GM-1881 move the licenses distribution to background to fix the timeout issue when distribute large emails/quantity

---
+++ app/workers/licenses_distribution_worker.rb
@@ -0,0 +1,14 @@
+class LicensesDistributionWorker
+  include Sidekiq::Worker
+
+  def perform(user_id, all_licenses_ids, all_emails, licenses_number)
+    user = Spree::User.find(user_id)
+    licensed_products = user.licensed_products.where(id: all_licenses_ids)
+    rows = all_emails.map do |email|
+      { email: email, quantity: licenses_number.to_i }
+    end
+
+    Spree::LicensesManager::LicensesDistributer.new(user: user, licensed_products: licensed_products, rows: rows).execute
+  end
+
+end
