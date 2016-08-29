0b2c3a2170d40c5f0ff38aaec66eed96a9c2f76c
Roc Yu <rociiu.yu@gmail.com>
2016-08-29 21:16:21 +0800

    Add test for Spree::UserRegistrationsController signup action

---
+++ spec/controllers/spree/user_registrations_controller_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe Spree::UserRegistrationsController, type: :controller do
+  include Spree::TestingSupport::ControllerRequests
+
+  before { @request.env['devise.mapping'] = Devise.mappings[:spree_user] }
+  let(:curriculum) { create(:curriculum, name: 'Math') }
+
+  describe "'POST' create" do
+    context "correct params" do
+      it "create user and redirect to terms" do
+        allow(RecordType).to receive(:find_in_salesforce_by_name_and_object_type).and_return(Hashie::Mash.new)
+        spree_post :create, spree_user: {
+          first_name: 'John',
+          last_name: 'Doe',
+          email: 'johndoe@example.com',
+          password: 'testfoo',
+          password_confirmation: 'testfoo',
+          title: 'Parent',
+          zip_code: '10000',
+          interested_subjects: [curriculum.id],
+          school_district_attributes: {},
+          school_id: '',
+          district_id: ''
+        }
+        expect(response).to redirect_to('/terms-of-service')
+        expect(Spree::User.last.email).to eq('johndoe@example.com')
+      end
+    end
+
+    context "incorrect params" do
+      it "render new" do
+        spree_post :create, spree_user: {
+          school_district_attributes: {},
+          school_id: '',
+          district_id: ''
+        }
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+end
