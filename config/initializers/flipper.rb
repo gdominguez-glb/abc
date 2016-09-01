70dae7347baaed22be5f9ba0069d847dd4e65634
Roc Yu <rociiu.yu@gmail.com>
2016-09-01 21:42:34 +0800

    Remove features configuration in feature flipper initializer which will reset the value on app start, all the features control should be from admin ui

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -5,19 +5,3 @@ client = Redis.new(url: ENV['REDIS_URL'])
 adapter = Flipper::Adapters::Redis.new(client)
 
 $flipper = Flipper.new(adapter)
-
-if Rails.env.development?
-  $flipper[:coupon_code].enable
-end
-
-if Rails.env.qa?
-  $flipper[:coupon_code].enable
-end
-
-if Rails.env.staging?
-  $flipper[:coupon_code].enable
-end
-
-if Rails.env.production?
-  $flipper[:coupon_code].enable
-end
