95331d892c3609865957b94e24f4e56597e27ccb
Roc Yu <rociiu0112@gmail.com>
2016-09-26 10:47:49 +0800

    Merge pull request #1592 from intridea/hotfix_checkout_exception
    Fix exception in production when un-login user checkout with a free o…

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,5 +1,6 @@
 Spree::CheckoutController.class_eval do
 
+  before_action :check_order_address, only: [:edit]
   before_action :check_user_state, only: [:edit]
 
   # override to custom page after complete order
@@ -11,6 +12,12 @@ Spree::CheckoutController.class_eval do
       @order.single_purchase? ? main_app.account_products_path : main_app.account_root_path
     else
       spree.completed_order_path(@order, custom_params)
+    end
+  end
+
+  def check_order_address
+    if params[:state] != 'address' && @order.checkout_steps.include?('address') && @order.bill_address.blank? && @order.ship_address.blank?
+      redirect_to spree.checkout_state_path(state: 'address') and return
     end
   end
 
