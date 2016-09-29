d653b9844dcdf7f842ded61b3fb2ff055902be24
Roc Yu <rociiu.yu@gmail.com>
2016-09-29 15:04:14 +0800

    Set page title in faq/blog/careers/newsletter/team pages for better seo

--- app/controllers/about/faq_controller.rb
+++ app/controllers/about/faq_controller.rb
@@ -1,9 +1,11 @@
 class About::FaqController < About::BaseController
   def index
     @faq_categories = FaqCategory.displayable.includes(:questions).order('position asc')
+    @page_title = 'FAQs'
   end
 
   def qa
     @question = Question.find(params[:id])
+    @page_title = "FAQ - #{@question.title}"
   end
 end
