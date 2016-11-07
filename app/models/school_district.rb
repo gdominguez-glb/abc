1930a84da0c857ea839ede554ffd24c7c12543f0
Roc Yu <rociiu.yu@gmail.com>
2016-11-07 19:01:03 +0800

    GM-2073 remove state name from school district dropdown

--- app/models/school_district.rb
+++ app/models/school_district.rb
@@ -178,6 +178,6 @@ class SchoolDistrict < ActiveRecord::Base
   }
 
   def name_in_option
-    [ name, [city.try(:name), state.try(:name)].compact.join(',') ].reject(&:blank?).join(' - ')
+    [ name, city.try(:name) ].reject(&:blank?).join(' - ')
   end
 end
