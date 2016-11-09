9bbff3acdd145b2f36060cd4bf960776299eba80
Roc Yu <rociiu0112@gmail.com>
2016-11-09 16:21:17 +0800

    Merge pull request #1681 from intridea/GM-2076_recommendation_by_zip_codes
    GM-2076 add zip codes to recommendations in cms and update dashboard …

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -4,13 +4,14 @@ class Recommendation < ActiveRecord::Base
   has_and_belongs_to_many :products, class_name: 'Spree::Product', join_table: 'products_recommendations'
 
   scope :with_subject, ->(subject){ where(subject: subject) }
-  scope :filter_by_subject_or_user_title, -> (subjects, user_title) {
-    if subjects.present? && user_title.present?
-      where("subject in (?) or user_title = ?", subjects, user_title)
-    elsif subjects.present?
-      where(subject: subjects)
-    elsif user_title.present?
-      where(user_title: user_title)
+  scope :filter_by_subject_or_user_title_or_zip_code, -> (subjects, user_title, zip_code) {
+    filter_conds = []
+    filter_conds << "(subject in (:subject))" if subjects.present?
+    filter_conds << "(user_title = :user_title)" if user_title.present?
+    filter_conds << "(zip_codes like :zip_code)" if zip_code.present?
+
+    if filter_conds.present?
+      where(filter_conds.join(" or "), subject: subjects, user_title: user_title, zip_code: "%#{zip_code}%")
     else
       where('1=1')
     end
--- spec/models/recommendation_spec.rb
+++ spec/models/recommendation_spec.rb
@@ -15,26 +15,26 @@ RSpec.describe Recommendation, type: :model do
     end
   end
 
-  describe ".filter_by_subject_or_user_title" do
+  describe ".filter_by_subject_or_user_title_or_zip_code" do
     before(:each) do
       @math_parent = create(:recommendation, subject: 'Math', user_title: 'Parent')
       @english_teacher = create(:recommendation, subject: 'English', user_title: 'Teacher')
     end
 
     it "filter by both subjects and user_title" do
-      expect(Recommendation.filter_by_subject_or_user_title(['Math'], 'Parent')).to eq([@math_parent])
+      expect(Recommendation.filter_by_subject_or_user_title_or_zip_code(['Math'], 'Parent', '')).to eq([@math_parent])
     end
 
     it "filter by only subjects" do
-      expect(Recommendation.filter_by_subject_or_user_title(['English'], nil)).to eq([@english_teacher])
+      expect(Recommendation.filter_by_subject_or_user_title_or_zip_code(['English'], nil, '')).to eq([@english_teacher])
     end
 
     it "filter by only user_title" do
-      expect(Recommendation.filter_by_subject_or_user_title([], 'Teacher')).to eq([@english_teacher])
+      expect(Recommendation.filter_by_subject_or_user_title_or_zip_code([], 'Teacher', '')).to eq([@english_teacher])
     end
 
     it "return all if all empty" do
-      expect(Recommendation.filter_by_subject_or_user_title([], nil).order(:subject)).to eq([@english_teacher, @math_parent])
+      expect(Recommendation.filter_by_subject_or_user_title_or_zip_code([], nil, '').order(:subject)).to eq([@english_teacher, @math_parent])
     end
   end
 
