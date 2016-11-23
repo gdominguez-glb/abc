006e83ceef3b3d361e599839be7a10500f7237c5
Roc Yu <rociiu.yu@gmail.com>
2016-11-23 09:15:48 +0800

    GM-2108 implement filter preferences on store when user click GO in home page to choose subject and role

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -2,6 +2,12 @@ Spree::HomeController.class_eval do
   include ProductTaxonsFilter
 
   def index
+    if params[:remove_all].present?
+      clear_preference_filters
+      redirect_to spree.root_path and return
+    end
+    apply_preference_filters if params[:taxon_ids].blank?
+
     @products = products_list_with_taxons_filter
 
     if params[:taxon_ids].blank?
@@ -10,5 +16,26 @@ Spree::HomeController.class_eval do
       @products = @products.sort_group_first
     end
     @products = @products.unexpire.unarchive.page(params[:page]).per(10)
+  end
+
+  def clear_preference_filters
+    session[:filter_role] = nil
+    session[:filter_curriculum] = nil
+  end
+
+  def apply_preference_filters
+    if session[:filter_role].present? && session[:filter_curriculum].present?
+      subject_id = find_taxon_by_taxonomy_and_name('Subject', session[:filter_curriculum])
+      role_id    = find_taxon_by_taxonomy_and_name('I am a...', session[:filter_role])
+      if subject_id && role_id
+        params[:taxon_ids] = [subject_id, role_id]
+      end
+    end
+  end
+
+  def find_taxon_by_taxonomy_and_name(taxonomy_name, taxon_name)
+    Spree::Taxonomy.show_in_store.find_by(name: taxonomy_name).taxons.find_by(name: taxon_name).id
+  rescue
+    nil
   end
 end
