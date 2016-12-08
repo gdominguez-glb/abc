7de27d6a1388c7f0f363764f6bcf28b3759a6adc
Roc Yu <rociiu.yu@gmail.com>
2016-12-08 10:15:51 +0800

    GM-2129 fix column type of famis_products grade need to store grade k too

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161208014552) do
+ActiveRecord::Schema.define(version: 20161208021322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -179,7 +179,7 @@ ActiveRecord::Schema.define(version: 20161208014552) do
     t.text     "small_description"
     t.text     "description"
     t.decimal  "price"
-    t.integer  "grade"
+    t.string   "grade"
     t.string   "isbn"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
