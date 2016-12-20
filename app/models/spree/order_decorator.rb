d857f4746ed5772ea58c0ed011d159b942889971
Roc Yu <rociiu.yu@gmail.com>
2016-12-20 15:30:28 +0800

    GM-2088 update user to salesforce when user purchase an order with bill/ship address to sync address on contact

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -224,6 +224,7 @@ Spree::Order.class_eval do
   def finalize_order
     create_in_salesforce
     create_licensed_products!
+    self.user.update_salesforce if self.user && (self.bill_address || self.ship_address)
     log_purchase_activity!
   end
 
