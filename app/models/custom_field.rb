49c074e50a8dab5779f12381146293bb46a964be
Roc Yu <rociiu.yu@gmail.com>
2017-01-13 09:33:07 +0800

    GM-2210 add CustomFieldOption into form as nested attributes form

--- app/models/custom_field.rb
+++ app/models/custom_field.rb
@@ -1,12 +1,12 @@
 class CustomField < ActiveRecord::Base
   include Displayable
 
-  FIELD_TYPES = ['select', 'multiple_select', 'text'].freeze
+  FIELD_TYPES = ['short_text', 'long_text', 'select', 'multiple_select'].freeze
 
   validates_presence_of :name, :description, :salesforce_field_name
 
   acts_as_list
 
   has_many :custom_field_options
-  accepts_nested_attributes_for :custom_field_options, allow_destroy: true
+  accepts_nested_attributes_for :custom_field_options, allow_destroy: true, reject_if: proc { |attributes| attributes['label'].blank? }
 end
