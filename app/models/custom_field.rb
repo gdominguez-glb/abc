06a862a3dee460ff2696684384740382f7a599ce
Roc Yu <rociiu.yu@gmail.com>
2017-01-13 09:50:00 +0800

    GM-2210 update subjects and role in custom field form to be multiple select

--- app/models/custom_field.rb
+++ app/models/custom_field.rb
@@ -7,6 +7,21 @@ class CustomField < ActiveRecord::Base
 
   acts_as_list
 
+  attr_accessor :subjects, :user_titles
+
+  after_initialize :set_subjects_and_user_titles
+  before_save :handle_subjects_and_user_titles
+
   has_many :custom_field_options
   accepts_nested_attributes_for :custom_field_options, allow_destroy: true, reject_if: proc { |attributes| attributes['label'].blank? }
+
+  def set_subjects_and_user_titles
+    self.subjects ||= (self.subject || '').split(',')
+    self.user_titles ||= (self.user_title || '').split(',')
+  end
+
+  def handle_subjects_and_user_titles
+    self.subject = self.subjects.reject(&:blank?).join(',')
+    self.user_title = self.user_titles.reject(&:blank?).join(',')
+  end
 end
