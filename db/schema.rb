9b8d20fcf3a83cc652c9b06bdf9163e663e14ea3
Roc Yu <rociiu.yu@gmail.com>
2017-01-17 15:48:52 +0800

    GM-2211 add effect date range on each custom fields, only show effective custom fields and when user dont fill the custom fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170113024526) do
+ActiveRecord::Schema.define(version: 20170117070541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,6 +114,8 @@ ActiveRecord::Schema.define(version: 20170113024526) do
     t.integer  "position",              default: 0
     t.datetime "created_at",                            null: false
     t.datetime "updated_at",                            null: false
+    t.datetime "effect_at"
+    t.datetime "expire_at"
   end
 
   create_table "document_taggings", force: :cascade do |t|
