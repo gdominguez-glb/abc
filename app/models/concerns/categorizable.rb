1652a0611f950eaae9a4552118b8063f34ba64ea
Lucas Juarez <lucasjavierjuarez@hotmail.com>
2017-02-02 16:55:47 -0300

    GM-2226 Create picklist in upload and vanity urls

---
+++ app/models/concerns/categorizable.rb
@@ -0,0 +1,5 @@
+module Categorizable
+  extend ActiveSupport::Concerns
+
+  UP_VAN = ["Parent", "Marketing", "Data", "Product", "Product Image", "PD", "Report", "Press", "Social Media", "Conference", "Other"].freeze
+end
--- spec/models/vanity_url_spec.rb
+++ spec/models/vanity_url_spec.rb
@@ -6,4 +6,6 @@ RSpec.describe VanityUrl, type: :model do
   it { should validate_presence_of(:redirect_url) }
   it { should_not allow_value('http://test.foo/').for(:url) }
   it { should_not allow_value('http://test.foo/store').for(:url) }
+
+  it_behaves_like "categorizable"
 end
