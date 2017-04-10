77c0cd944fed4f27060022caa17086ada644ac39
Lucas Juarez <lucasjavierjuarez@hotmail.com>
2017-04-10 15:16:59 -0300

    Revert "Merge pull request #1967 from intridea/GM-2349_english_content_error_mail"
    This reverts commit 035ea4e9b450508ba46748c73a52d110b662bdd7, reversing
    changes made to 9dec8d4526d300c17d4ce07996d3aa382703437d.

--- app/mailers/content_error_mailer.rb
+++
@@ -1,7 +0,0 @@
-class ContentErrorMailer < ApplicationMailer
-
-  def notify(content_error_info={})
-    @info = content_error_info
-    mail to: "errors.ww@greatminds.org", subject: "English Content Error"
-  end
-end
