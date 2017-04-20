9fca6f8725f9e5088345fd996204466537e3305a
Lucas Juarez <lucasjavierjuarez@hotmail.com>
2017-04-20 11:00:40 -0300

    GM-2373 create by field in blog

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419174016) do
+ActiveRecord::Schema.define(version: 20170420133207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20170419174016) do
     t.integer  "user_id"
     t.date     "publish_date"
     t.datetime "published_at"
+    t.string   "created_by"
   end
 
   create_table "blogs", force: :cascade do |t|
