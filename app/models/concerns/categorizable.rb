ce39ae1e4a9cf1ffa2f5ac1bf753d52cfd73a799
Roc Yu <rociiu.yu@gmail.com>
2017-05-05 09:17:24 +0800

    GM-2404 add Math and Engligh to uploads and vanity url categories

--- app/models/concerns/categorizable.rb
+++ app/models/concerns/categorizable.rb
@@ -1,5 +1,5 @@
 module Categorizable
   extend ActiveSupport::Concern
 
-  UP_VAN = ["Blog", "Parent", "Marketing", "Data", "Product", "Product Image", "PD", "Report", "Press", "Social Media", "Conference", "Other"].freeze
+  UP_VAN = ["Math", "English", "Blog", "Parent", "Marketing", "Data", "Product", "Product Image", "PD", "Report", "Press", "Social Media", "Conference", "Other"].freeze
 end
