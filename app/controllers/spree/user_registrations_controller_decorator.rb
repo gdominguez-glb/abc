111199be81d6266fef7323a775c19e138b089b9c
Lucas Juarez <lucasjavierjuarez@hotmail.com>
2017-05-08 13:25:27 -0300

    GM-2414 I'm not a robot
    rebase
    
    rebase
    
    rebase

--- app/controllers/spree/user_registrations_controller_decorator.rb
+++ app/controllers/spree/user_registrations_controller_decorator.rb
@@ -4,7 +4,7 @@ Spree::UserRegistrationsController.class_eval do
 
   # override to remove flash message on sign up
   def create
-    @user = build_resource(spree_user_params.merge(referral: session[:utm]))
+    @user = build_resource(spree_user_params.merge(referral: session[:utm], google_recaptcha_required: true, google_recaptcha: params["g-recaptcha-response"]))
     resource_saved = resource.save
     yield resource if block_given?
     if resource_saved
--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -16,6 +16,7 @@ Spree::User.class_eval do
   validates :zip_code, presence: true, on: :create, if: :is_in_usa?
   validates :interested_subjects, presence: true, on: :create
   before_create :set_city_and_state, if: :school_district_required?
+  validate :is_human?, on: :create
 
   belongs_to :delegate_for_user, class_name: 'Spree::User', foreign_key: :delegate_user_id
 
@@ -159,7 +160,7 @@ Spree::User.class_eval do
   has_many :download_jobs
   has_many :bookmarks
 
-  attr_accessor :school_id, :district_id, :ip_location
+  attr_accessor :school_id, :district_id, :ip_location, :google_recaptcha, :google_recaptcha_required
 
   accepts_nested_attributes_for :school_district, reject_if: proc { |attributes| attributes['name'].blank? }
 
@@ -245,5 +246,10 @@ Spree::User.class_eval do
 
   def set_city_and_state
     self.state = self.try(:school_district).try(:state).try(:name)
+  end
+
+  def is_human?
+    return unless google_recaptcha_required.present?
+    errors.add(:google_recaptcha, "You need to validate that you're not a robot") unless Google::Recaptcha.status(google_recaptcha)[:success]
   end
 end
