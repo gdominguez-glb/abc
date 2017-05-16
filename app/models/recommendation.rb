bcc804b0f8160d46e366c5dc4ba0bd1dfe8dac0d
Lucas Juarez <lucasjavierjuarez@hotmail.com>
2017-05-16 12:32:42 -0300

    GM-2423 update limit to 85 in recommendations subheader

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -2,7 +2,7 @@ class Recommendation < ActiveRecord::Base
 
   include Viewable, Clickable, Displayable, DashboardFilterable
   validates_presence_of :title, :sub_header, :call_to_action_button_text, :call_to_action_button_link
-  validates_length_of :sub_header, :maximum => 75
+  validates_length_of :sub_header, :maximum => 85
   has_and_belongs_to_many :products, class_name: 'Spree::Product', join_table: 'products_recommendations'
   scope :with_subject, ->(subject){ where(subject: subject) if subject.present? }
   scope :unexpired, -> { where('expire_at is null or expire_at > ?', Time.now) }
