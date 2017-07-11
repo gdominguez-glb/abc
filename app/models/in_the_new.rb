047375fa763c806c7b1f8a4c5f28799d0eb76299
Lucas Juarez <lucasjavierjuarez@hotmail.com>
2017-07-11 15:57:25 -0300

    GM-2479 reverse in the news

--- app/models/in_the_new.rb
+++ app/models/in_the_new.rb
@@ -6,6 +6,7 @@ class InTheNew < ActiveRecord::Base
   scope :search_by_publisher, ->(q) { where("publisher ilike ?", "%#{q}%") if q.present? }
   scope :search_by_article_date, ->(q) { where('article_date > ? AND article_date < ?', DateTime.parse(q).beginning_of_day, DateTime.parse(q).end_of_day) if q.present? }
   scope :search_by_description, ->(q) { where("description ilike ?", "%#{q}%") if q.present? }
+  scope :latest_by_article_date, -> { order(article_date: :desc) }
 
   searchkick callbacks: :async
 
