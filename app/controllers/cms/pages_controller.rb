78460badfd40d7515f49f86ed5745c704e374c44
Lucas Juarez <lucasjavierjuarez@hotmail.com>
2017-08-23 14:06:06 -0300

    GM-2518 add hubspot to each page

--- app/controllers/cms/pages_controller.rb
+++ app/controllers/cms/pages_controller.rb
@@ -144,7 +144,7 @@ module Cms
       params.require(:page).permit({seo_data: [:title, :description]}, :title, :seo_content, :slug, :group_name,
                                    :keywords, :description,
                                    :sub_group_name, :position, :layout, :body_draft,
-                                   :visible, :curriculum_id, :group_root,
+                                   :visible, :curriculum_id, :group_root, :hubspot,
                                    :show_in_nav, :show_in_footer, tiles: {})
     end
   end
