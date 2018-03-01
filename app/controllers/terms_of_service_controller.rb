cf145a842a162a2c4359e5ebd1005e116ff64895
Roc Yu <roc@mobomo.com>
2018-03-01 11:06:12 +0800

    GM-2656 add accepted_terms_2018 and accepted_terms_2018_at to record the updated terms accepted flag and time, update default signup terms to update new fields

--- app/controllers/terms_of_service_controller.rb
+++ app/controllers/terms_of_service_controller.rb
@@ -3,7 +3,7 @@ class TermsOfServiceController < ApplicationController
   skip_before_action :accepted_terms, only: [:display, :accept]
 
   def accept
-    spree_current_user.update(:accepted_terms => true) unless spree_current_user.blank?
+    spree_current_user.update(accepted_terms: true, accepted_terms_2018: true, accepted_terms_2018_at: Time.now) unless spree_current_user.blank?
     redirect_to (session["spree_user_return_to"] || account_products_path)
   end
 
