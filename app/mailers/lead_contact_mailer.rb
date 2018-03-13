5576dd06fa0e7c25598314569bd3920e3ae8ef10
Roc Yu <roc@mobomo.com>
2018-03-13 14:48:12 +0800

    GM-2643 add two more emails in the cc of email that send lead contact info

--- app/mailers/lead_contact_mailer.rb
+++ app/mailers/lead_contact_mailer.rb
@@ -1,6 +1,6 @@
 class LeadContactMailer < ApplicationMailer
   def notify(lead_sf_attrs)
     @lead_sf_attrs = lead_sf_attrs
-    mail to: 'julie.huston@greatminds.org', subject: 'Received new Lead'
+    mail to: 'julie.huston@greatminds.org', cc: ['jon@jonwilliams.com', 'david.blair@greatminds.org'], subject: 'Received new Lead'
   end
 end
