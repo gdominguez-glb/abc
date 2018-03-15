78d6f54be03f13b59cfddc0cfcf5b5a96dc07674
Roc Yu <rociiu0112@gmail.com>
2018-03-15 09:05:38 +0800

    Merge pull request #2248 from intridea/GM-2646_contact_excels_mail
    GM-2646 send leads contacts email with excels as attachment instead o…

--- app/mailers/lead_contact_mailer.rb
+++ app/mailers/lead_contact_mailer.rb
@@ -1,6 +1,6 @@
 class LeadContactMailer < ApplicationMailer
-  def notify(lead_sf_attrs)
-    @lead_sf_attrs = lead_sf_attrs
-    mail to: 'julie.huston@greatminds.org', subject: 'Received new Lead'
+  def notify(excel_file_path)
+    attachments["lead_contacts_#{Time.now.to_i}.xlsx"] = File.read(excel_file_path)
+    mail to: 'julie.huston@greatminds.org', cc: ['jon@jonwilliams.com', 'david.blair@greatminds.org'], subject: 'New leads'
   end
 end
