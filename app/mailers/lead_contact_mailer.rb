d760f372ca09386ed4980316c8dc1811835ecac4
Roc Yu <rociiu0112@gmail.com>
2018-03-23 09:38:01 +0800

    Merge pull request #2252 from intridea/GM-2650_update_contact_mail_emails
    GM-2650 update cc of lead contacts email

--- app/mailers/lead_contact_mailer.rb
+++ app/mailers/lead_contact_mailer.rb
@@ -1,6 +1,6 @@
 class LeadContactMailer < ApplicationMailer
   def notify(excel_file_path)
     attachments["lead_contacts_#{Time.now.to_i}.xlsx"] = File.read(excel_file_path)
-    mail to: 'julie.huston@greatminds.org', cc: ['info@greatminds.org', 'jon@jonwilliams.com', 'david.blair@greatminds.org'], subject: 'New leads'
+    mail to: 'julie.huston@greatminds.org', cc: ['angie.cox@greatminds.org', 'holly.hagen@greatminds.org'], subject: 'New leads'
   end
 end
