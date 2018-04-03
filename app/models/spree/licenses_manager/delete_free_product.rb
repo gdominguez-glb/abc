bd3519ea85c8653aec4e9091f745091c0532b14a
Roc Yu <roc@mobomo.com>
2018-04-03 08:43:03 +0800

    GM-2655 set the free product assets quantity in salesforce to zero when user delete the free product in web portal

--- app/models/spree/licenses_manager/delete_free_product.rb
+++ app/models/spree/licenses_manager/delete_free_product.rb
@@ -7,7 +7,10 @@ module Spree
       end
 
       def delete!
-        Spree::LicensedProduct.where(user_id: @user.id, product_id: @product.id).destroy_all
+        license_products = Spree::LicensedProduct.where(user_id: @user.id, product_id: @product.id).includes(:salesforce_reference)
+        sf_ids = license_products.map(&:id_in_salesforce)
+        license_products.destroy_all
+        ZeroSfAssetQuantityWorker.perform_async(sf_ids)
       end
     end
   end
