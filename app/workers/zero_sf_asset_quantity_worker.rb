795b9f8154a789c3faece3d66e9412283cc41552
Roc Yu <roc@mobomo.com>
2018-04-04 08:30:11 +0800

    GM-2655 fix error of worker that set zero quantity of asset in salesforce

--- app/workers/zero_sf_asset_quantity_worker.rb
+++ app/workers/zero_sf_asset_quantity_worker.rb
@@ -3,7 +3,7 @@ class ZeroSfAssetQuantityWorker
 
   def perform(sf_ids)
     client = GmSalesforce::Client.instance.client
-    sf_ids.compact.reject(&:blank).each do |sf_id|
+    sf_ids.compact.reject(&:blank?).each do |sf_id|
       client.update('Asset', Id: sf_id, Quantity: 0)
     end
   end
