b9f7a86eeaef51d7f1d7db837813ac2307c21e1f
Roc Yu <roc@mobomo.com>
2018-04-12 09:19:28 +0800

    GM-2662 fix the auto assign free products on signup due to issue on matching curriculum

--- app/models/spree/licenses_manager/auto_assign_free_products.rb
+++ app/models/spree/licenses_manager/auto_assign_free_products.rb
@@ -6,7 +6,7 @@ module Spree
       end
 
       def assign!
-        user_curriculums = @user.interested_curriculums.map(&:to_i)
+        user_curriculums = @user.interested_subjects.map(&:to_i)
         products = Spree::Product.where(auto_add_on_sign_up: true, title: @user.title).to_a.select{|p| p.free? && user_curriculums.include?(p.interested_curriculum_id) }
         FreeProductOrder.new(@user, products).save
       end
