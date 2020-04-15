0e613ed06ee290ae26d845763d93258484876512
rohit-lingayat-globant <Rohit.Lingayat@greatminds.org>
2020-04-15 14:30:47 +0530

    [CDS-1077] Added new test cases for blog controller

--- spec/factories/blogs.rb
+++ spec/factories/blogs.rb
@@ -1,13 +1,12 @@
 FactoryGirl.define do
   factory :blog do
-    title "MyString"
-blog_type 1
-position 1
-display false
-slug "MyString"
-header "MyString"
-description "MyText"
-page_id 1
+    title 'MyString'
+    blog_type 1
+    position 1
+    display false
+    slug 'MyString'
+    header 'MyString'
+    description 'MyText'
+    page_id 1
   end
-
 end
