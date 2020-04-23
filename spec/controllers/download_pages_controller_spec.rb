5dfa48297b298fc3270e84dd57e8a3cf6c744654
Luis Carlos Gonzalez <luiscarlos.gonzalez@greatminds.org>
2020-04-23 16:49:10 -0500

    [CDS-1097] Add test for locked products also mark a method for deprecation

--- spec/controllers/download_pages_controller_spec.rb
+++ spec/controllers/download_pages_controller_spec.rb
@@ -1,20 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe DownloadPagesController, type: :controller do
+  render_views
   let!(:download_page) { create(:download_page, slug: 'abc-download') }
 
   describe "without login" do
     it "redirect user without login" do
       get :show, slug: 'abc-download'
-      expect(response).to redirect_to('/store/login')
+      expect(response).to redirect_to('/resources/login')
     end
   end
 
   describe "with login" do
     let(:user) { create(:gm_user) }
     let!(:product) { create(:product) }
-    let!(:download_product) { create(:download_product, product: product, download_page: download_page) }
-    let!(:licensed_product) { create(:spree_licensed_product, user: user, product: product, quantity: 1, can_be_distributed: false) }
+    let!(:download_product) {
+      create(:download_product, product: product, download_page: download_page)
+    }
+    let!(:licensed_product) {
+      create(:spree_licensed_product, user: user,
+                                      product: product,
+                                      quantity: 1,
+                                      can_be_distributed: false)
+    }
 
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:spree_user]
@@ -32,6 +40,28 @@ RSpec.describe DownloadPagesController, type: :controller do
     it "redirect to not found if downlaod page not exists" do
       get :show, slug: 'non-exist-download-page'
       expect(response).to redirect_to('/not-found')
+    end
+  end
+
+  describe 'locked_product?' do
+    it 'returns true if the product is locked' do
+      user = create(:gm_user)
+      product = create(:product, price: 10)
+      download_product = create(:download_product, product: product,
+                                                   download_page: download_page)
+
+      sign_in :spree_user, user
+
+      get :show, slug: 'abc-download'
+
+      expect(response).to be_success
+      expect(response).to render_template('show')
+      expect(response.body).to include('panel-product locked')
+    end
+  end
+
+  describe 'bookmarked_material?' do
+    it 'returns true if the product is bookmarked material' do
     end
   end
 end
