0e433a6c3c055bcc2a37ada0d92d02c46dfacddd
rohit-lingayat-globant <Rohit.Lingayat@greatminds.org>
2020-09-02 10:52:45 +0530

    Added new feature branch

--- .github/scripts/generate_ssh.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p  /github/home/.ssh/
-openssl enc -d -aes-256-cbc -md sha512 -salt -in ./.github/actions/ruby-action/ssh_key -out /github/home/.ssh/id_rsa -k "$SSH_KEY" -a -pbkdf2
-echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
-chmod 600 /github/home/.ssh/id_rsa
