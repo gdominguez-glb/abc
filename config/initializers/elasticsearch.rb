68724e77a34117259330dea77b51b0967fd6a770
rohit-lingayat-globant <Rohit.Lingayat@greatminds.org>
2020-12-18 12:06:55 +0530

    Changing the configuration for ES

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,1 +1,6 @@
-Searchkick.client = Elasticsearch::Client.new(hosts: ["#{ENV['ELASTICSEARCH_URL'] || 'localhost'}:9200"], retry_on_failure: true)
+Searchkick.client = Elasticsearch::Client.new(
+  hosts: ["#{ENV['ELASTICSEARCH_URL'] || 'localhost'}:9200"],
+  retry_on_failure: true,
+  port: '9200',
+  scheme: 'https'
+)
